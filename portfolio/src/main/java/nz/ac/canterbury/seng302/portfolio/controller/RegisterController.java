3cbc1c438a6150a3a1adad18f644d49314a7643a
Aidan Campbell <aca170@uclive.ac.nz>
2022-03-05 12:42:37 +1300

    Cleaned up the register screen and enforced the mandatory fields. Also added some documentation to RegisterController. #feature #document

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/RegisterController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/RegisterController.java
@@ -3,6 +3,8 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 /**
  * Controller class for the register page
@@ -12,9 +14,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RegisterController {
 
+    /**
+     *
+     * @param model Parameters sent to thymeleaf template to be rendered into HTML
+     * @return Thymeleaf template for the register screen
+     */
     @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("test", "test"); //to remove just for testing
         return "register";
     }
+
+    //todo method to receive the inputs after registration and to perform checks on them, then redirect to account page
 }
