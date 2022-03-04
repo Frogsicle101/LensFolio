0479790dbdf744f20211220b49e25c94c432e49d
Sam Clark <scl113@uclive.ac.nz>
2022-03-04 19:08:45 +1300

    Working on User Account Services, specifically the retrieval of user data.
    Both the client and server sides of the UserAccount Services have the implementation of retrieving Account details. These could not be tested because registrations are required first

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/UserAccountsServerService.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/UserAccountsServerService.java
@@ -3,23 +3,29 @@ package nz.ac.canterbury.seng302.identityprovider.service;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import nz.ac.canterbury.seng302.identityprovider.User;
+import nz.ac.canterbury.seng302.identityprovider.UserRepository;
 import nz.ac.canterbury.seng302.shared.identityprovider.GetUserByIdRequest;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserAccountServiceGrpc.UserAccountServiceImplBase;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterRequest;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterResponse;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @GrpcService
 public class UserAccountsServerService extends UserAccountServiceImplBase {
 
+    @Autowired
+    private UserRepository repository;
 
     @Override
     public void getUserAccountById(GetUserByIdRequest request, StreamObserver<UserResponse> responseObserver) {
         UserResponse.Builder reply = UserResponse.newBuilder();
-        User user = DatabaseService.getUserfromDatabase(request.getId()); //ToDo This is broken because it should look up by ID
+        User user = repository.findById(request.getId());
 
         //Build UserResponse (proto) from User
         reply.setUsername(user.getUsername())
                 .setFirstName(user.getFirstName())
-                .setMiddleName(user.getMiddleName())
+//                .setMiddleName(user.getMiddleName())
                 .setLastName(user.getLastName())
                 .setNickname(user.getNickname())
                 .setBio(user.getBio())
@@ -32,4 +38,5 @@ public class UserAccountsServerService extends UserAccountServiceImplBase {
         responseObserver.onNext(reply.build());
         responseObserver.onCompleted();
     }
+    
 }
--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/UserAccountsClientService.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/UserAccountsClientService.java
@@ -1,9 +1,17 @@
 package nz.ac.canterbury.seng302.portfolio.service;
 
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import nz.ac.canterbury.seng302.shared.identityprovider.*;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserAccountsClientService
-{
+public class UserAccountsClientService {
+
+    @GrpcClient(value = "identity-provider-grpc-server")
+    private UserAccountServiceGrpc.UserAccountServiceBlockingStub userAccountStub;
+
+    public UserResponse getUserAccountById(GetUserByIdRequest request) {
+        return userAccountStub.getUserAccountById(request);
+    }
 
 }
