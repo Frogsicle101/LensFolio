4cd5d1dfa114a4ffd4d05039a6b2d2f3b6cca3a3
Aidan Campbell <aca170@uclive.ac.nz>
2022-03-04 15:24:38 +1300

    Created branch and register html screen and its controller #feature

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/RegisterController.java
@@ -0,0 +1,20 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller class for the register page
+ *
+ * This page is responsible for allowing users to register a new account
+ */
+@Controller
+public class RegisterController {
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("hello", "test"); //to remove just for testing
+        return "register";
+    }
+}
