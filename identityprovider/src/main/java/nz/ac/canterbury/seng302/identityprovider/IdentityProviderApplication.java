ed68aeeabc03b746ae5d6eb7e8de25d8cb2f24fb
Frederik Markwell <fma107@uclive.ac.nz>
2022-03-04 15:09:28 +1300

    Reworked database backend to use JPA and implemented authentication using new database system #feature #document

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/IdentityProviderApplication.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/IdentityProviderApplication.java
@@ -1,26 +1,15 @@
 package nz.ac.canterbury.seng302.identityprovider;
 
-import nz.ac.canterbury.seng302.identityprovider.service.DatabaseService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class IdentityProviderApplication {
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void setup() throws SQLException {
-        DatabaseService.setUpDatabase();
-
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(IdentityProviderApplication.class, args);
     }
-
 
 
 }
--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/User.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/User.java
@@ -1,9 +1,20 @@
 package nz.ac.canterbury.seng302.identityprovider;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private int id;
+
+    @Column(unique = true)
     private String username;
+
     private String password;
     private String firstName;
     private String lastName;
@@ -12,8 +23,9 @@ public class User {
     private String pronouns;
     private String email;
 
-    public User(int id, String username, String password, String firstName, String lastName, String nickname, String bio, String pronouns, String email) {
-        this.id = id;
+    protected User () {} // Used by JPA
+
+    public User(String username, String password, String firstName, String lastName, String nickname, String bio, String pronouns, String email) {
         this.username = username;
         this.password = password;
         this.firstName = firstName;
@@ -22,6 +34,11 @@ public class User {
         this.bio = bio;
         this.pronouns = pronouns;
         this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User [" + username + " (" + firstName + " " + lastName + ")]";
     }
 
     public int getId() {
