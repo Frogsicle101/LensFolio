8e75dbb88ba636c2fc0bddb346d38560b26fb2d7
Sam Clark <scl113@uclive.ac.nz>
2022-03-05 09:46:04 +1300

    Documented the UserAccounts services, both client and server side. #document

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/UserAccountsServerService.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/UserAccountsServerService.java
@@ -6,17 +6,29 @@ import nz.ac.canterbury.seng302.identityprovider.User;
 import nz.ac.canterbury.seng302.identityprovider.UserRepository;
 import nz.ac.canterbury.seng302.shared.identityprovider.GetUserByIdRequest;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserAccountServiceGrpc.UserAccountServiceImplBase;
-import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterRequest;
-import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterResponse;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * The UserAccountsServerService implements the server side functionality of the defined by the
+ * user_accounts.proto rpc contracts.
+ *
+ * @author Sam Clark
+ */
 @GrpcService
 public class UserAccountsServerService extends UserAccountServiceImplBase {
 
+    /** The repository where Users details are stored */
     @Autowired
     private UserRepository repository;
 
+    /**
+     * getUserAccountByID follows the gRPC contract and provides the server side service for retrieving
+     * user account details from the repository of users.
+     *
+     * @param request - The GetUserByIDRequest formatted to satisfy the user_accounts.proto gRPC
+     * @param responseObserver - used to return the response to the Client side of the service
+     */
     @Override
     public void getUserAccountById(GetUserByIdRequest request, StreamObserver<UserResponse> responseObserver) {
         UserResponse.Builder reply = UserResponse.newBuilder();
--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/UserAccountsClientService.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/UserAccountsClientService.java
@@ -4,12 +4,25 @@ import net.devh.boot.grpc.client.inject.GrpcClient;
 import nz.ac.canterbury.seng302.shared.identityprovider.*;
 import org.springframework.stereotype.Service;
 
+/**
+ * The UserAccountsClientServices class implements the functionality of the services outlined
+ * by the user_accounts.proto gRPC contacts. This allows the client to make requests to the server
+ * regarding their account.
+ *
+ * @author Sam Clark
+ */
 @Service
 public class UserAccountsClientService {
 
     @GrpcClient(value = "identity-provider-grpc-server")
     private UserAccountServiceGrpc.UserAccountServiceBlockingStub userAccountStub;
 
+    /**
+     * Sends a request to the UserAccountsServerService containing the id of a user, requesting the users account details.
+     *
+     * @param request - The request to send to the server, uses the GetUserByIDRequest message type defined by user_accounts.proto
+     * @return response - The servers response to the request, which follows the UserResponse message format.
+     */
     public UserResponse getUserAccountById(GetUserByIdRequest request) {
         return userAccountStub.getUserAccountById(request);
     }
