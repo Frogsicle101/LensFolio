d875f8c798448c754646884f11768c93a07fb353
Frederik Markwell <fma107@uclive.ac.nz>
2022-03-07 16:29:53 +1300

    Setup test user to be added to database automatically on startup

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/IdentityProviderApplication.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/IdentityProviderApplication.java
@@ -1,11 +1,34 @@
 package nz.ac.canterbury.seng302.identityprovider;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 @SpringBootApplication
 public class IdentityProviderApplication {
 
+    @Autowired
+    UserRepository repository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void setup() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        repository.save(new User(
+                "steve",
+                "password",
+                "Steve",
+                "Steveson",
+                "Stev",
+                "kdsflkdjf",
+                "Steve/Steve",
+                "steve@example.com"
+        ));
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(IdentityProviderApplication.class, args);
