39f459bd8e59cb5cb2ff9ecdae03974fb437d6cd
April <abc56@uclive.ac.nz>
2022-03-17 17:03:34 +1300

    Started logging implementation. Doesn't work when run through terminal with gradlew bootrun, but does work when the ide run configuration is used. #Feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/PortfolioApplication.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/PortfolioApplication.java
@@ -1,13 +1,21 @@
 package nz.ac.canterbury.seng302.portfolio;
 
+import nz.ac.canterbury.seng302.portfolio.controller.LoggingController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PortfolioApplication {
 
+    static Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
     public static void main(String[] args) {
+
+
         SpringApplication.run(PortfolioApplication.class, args);
+        logger.error("Eureka!");
     }
 
 }
