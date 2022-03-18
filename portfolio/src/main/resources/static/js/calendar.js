825c0ec1ae2948956ecf427300d5a9a115062203
Dongjin Zhou <dzh80@uclive.ac.nz>
2022-03-18 14:21:20 +1300

    show different color for project day

--- portfolio/src/main/resources/static/js/calendar.js
+++ portfolio/src/main/resources/static/js/calendar.js
@@ -2,8 +2,8 @@
   /*
    * Display calendar from current system time
    */
-  var dateObj = (function () {
-    var _date = new Date(); // current system date
+  let dateObj = (function () {
+    let _date = new Date(); // current system date
     return {
       getDate: function () {
         return _date;
@@ -25,9 +25,9 @@
    * setting calendar div in html
    */
   function renderHtml() {
-    var calendar = document.getElementById("calendar");
-    var titleBox = document.createElement("div"); // Display the current month, and change the month
-    var bodyBox = document.createElement("div"); // Displays every day of the month
+    let calendar = document.getElementById("calendar");
+    let titleBox = document.createElement("div"); // Display the current month, and change the month
+    let bodyBox = document.createElement("div"); // Displays every day of the month
 
     // setting titleBox
     titleBox.className = "calendar-title-box";
@@ -39,7 +39,7 @@
 
     // setting bodyBox
     bodyBox.className = "calendar-body-box";
-    var _headHtml =
+    let _headHtml =
       "<tr>" +
       "<th>Su</th>" +
       "<th>Mo</th>" +
@@ -49,10 +49,10 @@
       "<th>Fr</th>" +
       "<th>Sa</th>" +
       "</tr>";
-    var _bodyHtml = "";
+    let _bodyHtml = "";
 
     // setting 6 lines for 6 weeks
-    for (var i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
       _bodyHtml +=
         "<tr>" +
         "<td></td>" +
@@ -74,38 +74,59 @@
   }
 
   /**
+   * chech it is during project date or not
+   */
+  function isDuringDate(beginDateStr, endDateStr, dateCheckStr) {
+    let curDate = new Date(dateCheckStr),
+      beginDate = new Date(beginDateStr),
+      endDate = new Date(endDateStr);
+    if (curDate >= beginDate && curDate <= endDate) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
    * display numbers in table
    */
-  function showCalendarData() {
-    var _year = dateObj.getDate().getFullYear();
-    var _month = dateObj.getDate().getMonth() + 1;
-    var _dateStr = getDateStr(dateObj.getDate());
+  function showCalendarData(beginDateStr, endDateStr) {
+    let _year = dateObj.getDate().getFullYear();
+    let _month = dateObj.getDate().getMonth() + 1;
+    let _dateStr = getDateStr(dateObj.getDate());
 
     // setting calendar title information
-    var calendarTitle = document.getElementById("calendarTitle");
-    var titleStr = _dateStr.substr(0, 4) + " - " + _dateStr.substr(4, 2);
+    let calendarTitle = document.getElementById("calendarTitle");
+    let titleStr = _dateStr.substr(0, 4) + " - " + _dateStr.substr(4, 2);
     calendarTitle.innerText = titleStr;
 
     // setting numbers in body table
-    var _table = document.getElementById("calendarTable");
-    var _tds = _table.getElementsByTagName("td");
-    var _firstDay = new Date(_year, _month - 1, 1); // first day in current month
-    for (var i = 0; i < _tds.length; i++) {
-      var _thisDay = new Date(_year, _month - 1, i + 1 - _firstDay.getDay());
-      var _thisDayStr = getDateStr(_thisDay);
+    let _table = document.getElementById("calendarTable");
+    let _tds = _table.getElementsByTagName("td");
+    let _firstDay = new Date(_year, _month - 1, 1); // first day in current month
+    for (let i = 0; i < _tds.length; i++) {
+      let _thisDay = new Date(_year, _month - 1, i + 1 - _firstDay.getDay());
+      let _thisDayStr = getDateStr(_thisDay);
       _tds[i].innerText = _thisDay.getDate();
       //_tds[i].data = _thisDayStr;
       _tds[i].setAttribute("data", _thisDayStr);
       if (_thisDayStr == getDateStr(new Date())) {
         // current day
-        _tds[i].className = "currentDay";
-      } else if (
-        _thisDayStr.substr(0, 6) == getDateStr(_firstDay).substr(0, 6)
-      ) {
-        _tds[i].className = "currentMonth"; // current month
+        _tds[i].className = "currentDay_project";
+      }
+      if (isDuringDate(beginDateStr, endDateStr, _thisDayStr)) {
+        if (_thisDayStr == getDateStr(new Date())) {
+          // current day + project date
+          _tds[i].className = "currentDay_project";
+        } else {
+          _tds[i].className = "project-bgcolor"; // project date
+        }
       } else {
-        // other month
-        _tds[i].className = "otherMonth";
+        if (_thisDayStr == getDateStr(new Date())) {
+          // current day + no project date
+          _tds[i].className = "currentDay_no_project";
+        } else {
+          _tds[i].className = "no-project-bgcolor"; // not project date
+        }
       }
     }
   }
@@ -114,8 +135,8 @@
    * Bind previous month to next month events
    */
   function bindEvent() {
-    var prevMonth = document.getElementById("prevMonth");
-    var nextMonth = document.getElementById("nextMonth");
+    let prevMonth = document.getElementById("prevMonth");
+    let nextMonth = document.getElementById("nextMonth");
     addEvent(prevMonth, "click", toPrevMonth);
     addEvent(nextMonth, "click", toNextMonth);
   }
@@ -146,7 +167,7 @@
    * Click the previous month icon
    */
   function toPrevMonth() {
-    var date = dateObj.getDate();
+    let date = dateObj.getDate();
     dateObj.setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1));
     showCalendarData();
   }
@@ -155,7 +176,7 @@
    * Click the next month icon
    */
   function toNextMonth() {
-    var date = dateObj.getDate();
+    let date = dateObj.getDate();
     dateObj.setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1));
     showCalendarData();
   }
@@ -164,9 +185,9 @@
    * data format
    */
   function getDateStr(date) {
-    var _year = date.getFullYear();
-    var _month = date.getMonth() + 1; // month start form 0
-    var _d = date.getDate();
+    let _year = date.getFullYear();
+    let _month = date.getMonth() + 1; // month start form 0
+    let _d = date.getDate();
 
     _month = _month > 9 ? "" + _month : "0" + _month;
     _d = _d > 9 ? "" + _d : "0" + _d;
