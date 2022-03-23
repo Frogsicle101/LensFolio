8a3fce3c2460dd816473948d6ba1062b7b17c8f4
Harrison Martin <hma112@uclive.ac.nz>
2022-03-23 13:40:36 +1300

    Updated frontend/backend to allow put requests from frontend to create a new event.
    #feature

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EventController.java
@@ -0,0 +1,45 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import nz.ac.canterbury.seng302.portfolio.events.Event;
+import nz.ac.canterbury.seng302.portfolio.events.EventRepository;
+import nz.ac.canterbury.seng302.portfolio.projects.ProjectRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class EventController {
+
+    private final ProjectRepository projectRepository;
+    private final EventRepository eventRepository;
+
+    public EventController(ProjectRepository projectRepository, EventRepository eventRepository) {
+        this.projectRepository = projectRepository;
+        this.eventRepository = eventRepository;
+    }
+
+    @PutMapping("/addEvent")
+    public ResponseEntity<String> deleteSprint(
+            @RequestParam(value = "projectId") Long projectId,
+            @RequestParam(value = "eventName") String name,
+            @RequestParam(value = "eventStart")  String start,
+            @RequestParam(value = "eventEnd") String end
+    ) {
+        try {
+            LocalDateTime eventStart = LocalDateTime.parse(start, DateTimeFormatter.ISO_DATE_TIME);
+            LocalDateTime eventEnd = LocalDateTime.parse(end, DateTimeFormatter.ISO_DATE_TIME);
+
+            Event event = new Event(projectId, name, eventStart, eventEnd);
+            eventRepository.save(event);
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        } catch(Exception err) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+}
--- portfolio/src/main/resources/static/js/project.js
+++ portfolio/src/main/resources/static/js/project.js
@@ -4,30 +4,24 @@ $(document).ready(() => {
 
 
 
-    // Project buttons
+    // Project button edit
     $("#projectEditSprint").click(() => {
         location.href = "/editProject?projectId=" + projectId;
     })
-
+    // Project button add
     $("#projectAddSprint").click(function () {
         location.href = "/portfolio/addSprint?projectId=" + projectId;
     })
 
 
 
-
-
-
-
-
-
-    // Sprint Buttons
+    // Sprint Button Edit
     $(".editSprint").click(function () {
         let sprintId = $(this).closest(".sprint").find(".sprintId").text();
         location.href = "/sprintEdit?sprintId=" + $(this).closest(".sprint").find(".sprintId").text();
     })
 
-
+    // Sprint Button delete
     $(".deleteSprint").click(function() {
         let sprintId = $(this).closest(".sprint").find(".sprintId").text();
         $.ajax({
@@ -37,8 +31,33 @@ $(document).ready(() => {
         }).done(function () {
             location.href = "/portfolio"
         })
-
     })
+
+    $(".addEventButton").click(function() {
+        $(".eventForm").slideToggle();
+    })
+
+    $("#eventSubmit").click(function(event) {
+        event.preventDefault();
+        let eventData = {
+            "projectId": projectId,
+            "eventName": $("#eventName").val(),
+            "eventStart": $("#eventStart").val(),
+            "eventEnd": $("#eventEnd").val()
+        }
+        $.ajax({
+            url: "/addEvent",
+            type: "put",
+            data: eventData,
+            success: function(response) {
+                location.href = "/portfolio"
+            },
+            error: function(response) {
+
+            }
+        })
+    })
+
 
 
 
