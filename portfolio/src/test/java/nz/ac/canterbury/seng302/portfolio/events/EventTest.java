44813c2cf6ba4725a417522ed0a9bbdddb73e6c1
Harrison Martin <hma112@uclive.ac.nz>
2022-03-25 12:39:24 +1300

    Added in more testing to EventTest.java #test

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/events/EventTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/events/EventTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -44,6 +45,31 @@ class EventTest {
 
         // Then
         assertThat(event.getId()).isEqualTo(foundEvent.getId());
+
+
+    }
+
+    @Test
+    void testWhenDeleteById_ThenReturnListOfEventsWithoutDeletedEvent(){
+        // Given
+        Project project = projectRepository.getProjectByName("Project Default");
+        Event event = new Event(project, "TestEvent", LocalDateTime.now(), LocalDateTime.now().plusDays(1));
+        Event event2 = new Event(project, "TestEvent2", LocalDateTime.now(), LocalDateTime.now().plusDays(1));
+        Event event3 = new Event(project, "TestEvent3", LocalDateTime.now(), LocalDateTime.now().plusDays(1));
+        entityManager.persist(event);
+        entityManager.persist(event2);
+        entityManager.persist(event3);
+        entityManager.flush();
+
+
+        // When
+        eventRepository.deleteById(event.getId());
+        List<Event> eventList = (List<Event>) eventRepository.findAll();
+
+        // Then
+        assertThat(event).isNotIn(eventList);
+        assertThat(event2).isIn(eventList);
+        assertThat(event3).isIn(eventList);
 
 
     }
