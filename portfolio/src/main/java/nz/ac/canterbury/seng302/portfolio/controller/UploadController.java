ca8507f2fad958407744593d03a4592ba294f931
Frederik Markwell <fma107@uclive.ac.nz>
2022-03-27 17:07:59 +1300

    Edited html and controller so that form conforms to UI standards #feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/UploadController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/UploadController.java
@@ -1,17 +1,37 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountsClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UploadController {
+
+    @Autowired
+    UserAccountsClientService userAccountsClientService;
 
     /**
      * Shows the form to upload a profile image if the user is logged in
      * @return The Thymeleaf upload html template.
      */
     @GetMapping("/upload")
-    public String showUpload() {
+    public String showUpload(
+            @AuthenticationPrincipal AuthState principal,
+            HttpServletRequest request,
+            Model model
+    ) {
+        UserResponse user = PrincipalAttributes.getUserFromPrincipal(principal, userAccountsClientService);
+
+        String ip = request.getLocalAddr();
+        String url = "http://" + ip + ":9001/" + user.getProfileImagePath();
+        model.addAttribute("profileImageUrl", url);
         return "upload-image";
     }
 
