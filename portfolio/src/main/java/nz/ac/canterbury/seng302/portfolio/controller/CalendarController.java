8e0582a12669be3bdabe06ea86df3a230d194d28
Sam Clark <scl113@uclive.ac.nz>
2022-03-30 13:15:58 +1300

    #Fix added the profile image to the header of the calendar page

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/CalendarController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/CalendarController.java
@@ -2,15 +2,21 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 
 import nz.ac.canterbury.seng302.portfolio.projects.Project;
 import nz.ac.canterbury.seng302.portfolio.projects.ProjectRepository;
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountsClientService;
 import nz.ac.canterbury.seng302.portfolio.sprints.Sprint;
 import nz.ac.canterbury.seng302.portfolio.sprints.SprintRepository;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -19,17 +25,27 @@ public class CalendarController {
     private final ProjectRepository projectRepository;
     private final SprintRepository sprintRepository;
 
+    @Autowired
+    private UserAccountsClientService userAccountsClientService;
+
     public CalendarController(ProjectRepository projectRepository, SprintRepository sprintRepository) {
         this.projectRepository = projectRepository;
         this.sprintRepository = sprintRepository;
     }
 
     @GetMapping("/calendar")
-    public ModelAndView getCalendar() {
+    public ModelAndView getCalendar(
+            @AuthenticationPrincipal AuthState principal,
+            HttpServletRequest request
+            ) {
         ModelAndView model = new ModelAndView("monthly_calendar");
         Project project = projectRepository.getProjectById(1L);
         model.addObject("project", project);
         model.addObject("sprints", sprintRepository.findAllByProjectId(project.getId()));
+        UserResponse user = PrincipalAttributes.getUserFromPrincipal(principal, userAccountsClientService);
+        String ip = request.getLocalAddr();
+        String url = "http://" + ip + ":9001/" + user.getProfileImagePath();
+        model.addObject("profileImageUrl", url);
 
         return model;
     }
