3491054bd5a2d056518ef5fd26f572b9b4c28308
Samuel Clark <scl113@uclive.ac.nz>
2022-04-16 16:24:30 +1200

    #Test Wrote the tests for uploading and setting user profile images, as well as retrieving them back

--- identityprovider/src/test/java/cucumber/User_Profile_Photos/ProfilePhotoStepDefinitions.java
+++ identityprovider/src/test/java/cucumber/User_Profile_Photos/ProfilePhotoStepDefinitions.java
@@ -1,40 +1,45 @@
 package cucumber.User_Profile_Photos;
 
+import com.google.protobuf.ByteString;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.grpc.stub.StreamObserver;
 import nz.ac.canterbury.seng302.identityprovider.User;
 import nz.ac.canterbury.seng302.identityprovider.UserRepository;
-import nz.ac.canterbury.seng302.identityprovider.service.ProfilePhotoService;
+import nz.ac.canterbury.seng302.identityprovider.service.ImageRequestStreamObserver;
 import nz.ac.canterbury.seng302.identityprovider.service.TimeService;
-import org.junit.jupiter.api.BeforeEach;
+import nz.ac.canterbury.seng302.shared.identityprovider.ProfilePhotoUploadMetadata;
+import nz.ac.canterbury.seng302.shared.identityprovider.UploadUserProfilePhotoRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.*;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ProfilePhotoStepDefinitions {
 
     @MockBean
-    @Autowired
     UserRepository repository = mock(UserRepository.class);
 
-    User user;
+    private MockImageResponseStreamObserver mockImageResponseStreamObserver = new MockImageResponseStreamObserver();
 
-    int userId;
+    private User user;
 
-    URL path;
+    private int userId;
 
-    URL testImagePath;
+    private URL receivedImagePath;
+
+    private URL expectedImagePath;
 
     @Given("I am logged in as user id {int}")
     public void i_am_logged_in_as_user_id(int userId) {
@@ -59,34 +64,80 @@ public class ProfilePhotoStepDefinitions {
     public void i_have_no_profile_photo() {
         user.deleteProfileImage();
     }
+
+
     @When("I request my profile photo Image")
     public void i_request_my_profile_photo_image() {
-        path = user.getProfileImagePath();
+        receivedImagePath = user.getProfileImagePath();
     }
+
+
     @Then("I receive the default profile photo icon")
     public void i_receive_the_default_profile_photo_icon() throws MalformedURLException {
-        URL expectedPath = new URL("http", "localhost", 9001, "profile/default.png");
-
-        assertEquals(expectedPath, path);
+        expectedImagePath = new URL("http", "localhost", 9001, "profile/default.png");
+        assertEquals(expectedImagePath, receivedImagePath);
     }
+
 
     @When("I change my profile photo")
     public void i_change_my_profile_photo() {
         try {
-            testImagePath = new URL("http", "localhost", 9001, "/profile/" + 1 + ".jpg");
+            URL resource = ProfilePhotoStepDefinitions.class.getResource("/testProfileImage.jpg");
+            String imagePath = Paths.get(resource.toURI()).toFile().getAbsolutePath();
+
+            uploadTestProfileImage(imagePath, "jpg");
+            expectedImagePath = new URL("http", "localhost", 9001, "/profile/" + 1 + ".jpg");
         } catch (MalformedURLException exception) {
             fail("Invalid URL setting");
+        } catch (IOException exception) {
+            fail("Failed to upload the new image");
+        } catch (URISyntaxException exception) {
+            fail("Failed to Convert image path");
+        } catch (NullPointerException exception) {
+            fail("Failed to find test image resource");
         }
-        ProfilePhotoService profilePhotoService = new ProfilePhotoService();
-        profilePhotoService.setRepository(repository);
-        profilePhotoService.updateProfileImage(userId, testImagePath);
     }
+
 
     @Then("I receive the profile photo for id {int}")
     public void i_receive_the_profile_photo_for_id(int userId) {
-        User user = repository.findById(userId);
-        URL receivedFilePath = user.getProfileImagePath();
-        assertEquals(testImagePath, receivedFilePath);
+        receivedImagePath = repository.findById(userId).getProfileImagePath();
+        assertEquals(expectedImagePath, receivedImagePath);
+    }
+
+
+
+    private void uploadTestProfileImage(String newImage, String fileType) throws IOException {
+        when(repository.save(user)).thenReturn(null);
+
+        ArrayList<UploadUserProfilePhotoRequest> requestChunks = new ArrayList<>();
+
+        ProfilePhotoUploadMetadata metadata = ProfilePhotoUploadMetadata.newBuilder()
+                .setUserId(userId)
+                .setFileType(fileType)
+                .build();
+
+
+        requestChunks.add(UploadUserProfilePhotoRequest.newBuilder()
+                .setMetaData(metadata)
+                .build()
+        );
+
+        InputStream photo = new BufferedInputStream(new FileInputStream(newImage));
+
+        byte[] bytes = new byte[4096];
+        int size;
+        while ((size = photo.read(bytes)) > 0){
+            UploadUserProfilePhotoRequest uploadRequest = UploadUserProfilePhotoRequest.newBuilder()
+                    .setFileContent(ByteString.copyFrom(bytes, 0 , size))
+                    .build();
+            requestChunks.add(uploadRequest);
+        }
+        photo.close();
+
+        StreamObserver<UploadUserProfilePhotoRequest> requestObserver = new ImageRequestStreamObserver(mockImageResponseStreamObserver, repository);
+        mockImageResponseStreamObserver.initialise(requestObserver);
+        mockImageResponseStreamObserver.sendImage(requestChunks);
     }
 
 }
--- identityprovider/src/test/resources/features/TestAddUserProfilePhoto.feature
+++ identityprovider/src/test/resources/features/TestAddUserProfilePhoto.feature
@@ -4,4 +4,5 @@ Feature: Add User Profile Photo
     Given I am logged in as user id 1
     And I have no profile photo
     When I change my profile photo
+    And I request my profile photo Image
     Then I receive the profile photo for id 1
