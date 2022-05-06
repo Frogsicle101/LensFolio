0ab436a1092dcf6e6ecaf09cb23596417f1a0e4a
=April Clarke <abc56@uclive.ac.nz>
2022-05-06 15:37:09 +1200

    Added date sorting to repo queries for milestones and deadlines #feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/projects/deadlines/DeadlineRepository.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/projects/deadlines/DeadlineRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface DeadlineRepository extends CrudRepository<Deadline, UUID> {
-    @Query("select d from #{#entityName} as d where d.startDate IS NULL and d.project.id = ?1")
+    @Query("select d from #{#entityName} as d where d.startDate IS NULL and d.project.id = ?1 order by d.dateTime")
     List<Deadline> findAllByProjectId(Long projectId);
 }
