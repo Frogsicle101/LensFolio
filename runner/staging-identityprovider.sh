16635eb29d251f6f0ed32fd67fbc80d735a223ec
Frederik Markwell <fma107@uclive.ac.nz>
2022-05-06 15:29:01 +1200

    Fixing incorrect number from previous push #fix

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -3,7 +3,7 @@ fuser -k 9502/tcp || true
 java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
   --spring.application.name=identity-provider \
   --grpc.server.port=9502 \
-  --server.port=9501 \
+  --server.port=9500 \
   --protocol=https \
   --hostName=csse-s302g6.canterbury.ac.nz \
   --port=443
