ee00b835039f34925d42ea9e07363cf69dbcc7cd
Frederik Markwell <fma107@uclive.ac.nz>
2022-05-06 13:51:54 +1200

    Updated the identity provider shell script, changing the web server port it uses #chore

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -1,2 +1,3 @@
 fuser -k 9500/tcp || true
-java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar --spring.application.name=identity-provider --grpc.server.port=9500
+fuser -k 9502/tcp || true
+java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar --spring.application.name=identity-provider --grpc.server.port=9500 --server.port=9502
