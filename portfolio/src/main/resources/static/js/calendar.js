ca422f738259ec23a678c2914306a244f2bdcc08
Sam Clark <scl113@uclive.ac.nz>
2022-05-09 14:20:05 +1200

    removed /'s from the calendar.js and projectEdit.js files to attempt to fix VM

--- portfolio/src/main/resources/static/js/calendar.js
+++ portfolio/src/main/resources/static/js/calendar.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
     eventResize,
     themeSystem: 'bootstrap5',
     eventSources: [{ //The sources to grab the events from.
-      url: '/getProjectSprintsWithDatesAsFeed', //Project sprints
+      url: 'getProjectSprintsWithDatesAsFeed', //Project sprints
       method: "get",
       extraParams: {
                 projectId: projectId.toString()
@@ -47,7 +47,7 @@ $(document).ready(function() {
       }
     },
       {
-        url: '/getProjectAsFeed', // Project itself
+        url: 'getProjectAsFeed', // Project itself
         method: "get",
         display: "inverse-background",
         extraParams: {
