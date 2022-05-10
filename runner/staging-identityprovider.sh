89bc4873d6498f125e23b380b5cec9918087756b
Frederik Markwell <fma107@uclive.ac.nz>
2022-05-10 15:48:24 +1200

    Attempt to pass the username and password in a third way #fix

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -1,5 +1,10 @@
 fuser -k 9500/tcp || true
 fuser -k 9502/tcp || true
+
+DATABASE_USERNAME=$(<staging-identityprovider/username.txt)
+DATABASE_PASSWORD=$(<staging-identityprovider/password.txt)
+
+
 java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
   --spring.application.name=identity-provider \
   --grpc.server.port=9502 \
@@ -11,4 +16,6 @@ java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
   --photoLocation=staging-identityprovider/photos/ \
   --spring.datasource.url=jdbc:mariadb://db2.csse.canterbury.ac.nz/seng302-2022-team600-identityprovider-test \
   --spring.datasource.driver-class-name=org.mariadb.jdbc.Driver \
-  --spring.jpa.hibernate.ddl-auto=create-drop
+  --spring.jpa.hibernate.ddl-auto=create-drop \
+  --spring.datasource.username=${DATABASE_USERNAME} \
+  --spring.datasource.password=${DATABASE_PASSWORD}
