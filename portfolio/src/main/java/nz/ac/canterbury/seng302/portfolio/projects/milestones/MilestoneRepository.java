9ccd0a21b98e4fb0123091e9eb4452dbe133e126
Harrison Martin <hma112@uclive.ac.nz>
2022-05-10 21:08:35 +1200

    Push to test if fixes

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/projects/milestones/MilestoneRepository.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/projects/milestones/MilestoneRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 import java.util.UUID;
 
-public interface MilestoneRepository extends CrudRepository<Milestone, UUID> {
+public interface MilestoneRepository extends CrudRepository<Milestone, String> {
     @Query("select m from #{#entityName} as m where m.startDate IS NULL and m.dateTime IS NULL and m.project.id = ?1 order by m.endDate")
     List<Milestone> findAllByProjectIdOrderByEndDate(Long projectId);
 
