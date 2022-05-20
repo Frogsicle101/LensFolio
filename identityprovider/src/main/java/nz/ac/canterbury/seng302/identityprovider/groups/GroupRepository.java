6738aa83aaa7a7f743bf4379116c7290810844c8
Samuel Clark <scl113@uclive.ac.nz>
2022-05-20 16:38:27 +1200

    #Fix moved the groups storage and services and test to the IdP

---
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/groups/GroupRepository.java
@@ -0,0 +1,16 @@
+package nz.ac.canterbury.seng302.identityprovider.groups;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface GroupRepository extends CrudRepository<Group, Integer> {
+
+    @Query
+    Optional<Group> findByShortName(String shortName);
+
+    @Query
+    Optional<Group> findByLongName(String longName);
+
+}
--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/groups/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package nz.ac.canterbury.seng302.portfolio.groups;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface GroupRepository extends CrudRepository<Group, Long> {
-
-}
