54b30c9c51d16a6d182daa9b89039754b539f1b3
Daniel Pallesen <dpa107@uclive.ac.nz>
2022-05-24 11:48:28 +1200

    Added some more API call functions and added to test page. #Feature

--- portfolio/src/main/resources/static/js/gitlabAPI.js
+++ portfolio/src/main/resources/static/js/gitlabAPI.js
@@ -1,9 +1,52 @@
+let projectInput;
+let tokenInput
+let shaInput;
+
+document.addEventListener('DOMContentLoaded', function() {
+    projectInput = document.getElementById("apiProjectID");
+    tokenInput = document.getElementById("apiAccessToken");
+    shaInput = document.getElementById("apiCommitSha");
+}, false);
 
 
-function getBranches(repoID) {
-    $.get("https://eng-git.canterbury.ac.nz/api/v4/projects/" + repoID + "/repository/branches", function(data, status){
-        alert(data + " " + status);
+
+function getBranches() {
+    let repoID = projectInput.value;
+    let accessToken = tokenInput.value;
+    $.get("https://eng-git.canterbury.ac.nz/api/v4/projects/" + repoID + "/repository/branches?access_token=" + accessToken, function(data, status){
+        console.log(JSON.stringify(data) + " " + status)
+        alert(JSON.stringify(data) + " " + status);
+        return data;
     });
 }
 
-getBranches("12298");
+function getCommits() {
+    let repoID = projectInput.value;
+    let accessToken = tokenInput.value;
+    $.get("https://eng-git.canterbury.ac.nz/api/v4/projects/" + repoID + "/repository/commits?access_token=" + accessToken, function(data, status){
+        console.log(JSON.stringify(data) + " " + status)
+        alert(JSON.stringify(data) + " " + status);
+        return data;
+    });
+}
+
+function getCommit() {
+    let repoID = projectInput.value;
+    let accessToken = tokenInput.value;
+    let commitSha = shaInput.value;
+    $.get("https://eng-git.canterbury.ac.nz/api/v4/projects/" + repoID + "/repository/commits/" + commitSha + "?access_token=" + accessToken, function(data, status){
+        console.log(JSON.stringify(data) + " " + status)
+        alert(JSON.stringify(data) + " " + status);
+        return data;
+    });
+}
+
+function getMembers() {
+    let repoID = projectInput.value;
+    let accessToken = tokenInput.value;
+    $.get("https://eng-git.canterbury.ac.nz/api/v4/projects/" + repoID + "/members?access_token=" + accessToken, function(data, status){
+        console.log(JSON.stringify(data) + " " + status)
+        alert(JSON.stringify(data) + " " + status);
+        return data;
+    });
+}
