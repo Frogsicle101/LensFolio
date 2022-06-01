798edb030b18da7e6d87ff1699afdf00c612be84
Harrison Martin <hma112@uclive.ac.nz>
2022-06-02 09:20:10 +1200

    All tests have been fixed.
    #fix

--- identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/GroupServiceTest.java
+++ identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/GroupServiceTest.java
@@ -1,79 +1,105 @@
-//package nz.ac.canterbury.seng302.identityprovider.service;
-//
-//import nz.ac.canterbury.seng302.identityprovider.UserRepository;
-//import nz.ac.canterbury.seng302.identityprovider.groups.Group;
-//import nz.ac.canterbury.seng302.identityprovider.groups.GroupRepository;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//
-//import java.util.ArrayList;
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.Mockito.when;
-//
-//
-//class GroupServiceTest {
-//
-//    private final GroupRepository groupRepository = Mockito.mock(GroupRepository.class);
-//    private final UserRepository userRepository = Mockito.mock(UserRepository.class);
-//
-//    private GroupService groupService;
-//
-//    @BeforeEach
-//    public void setUp() {
-//
-//        groupService = new GroupService(groupRepository, userRepository);
-//    }
-//
-//    @Test
-//    void testAddUser() {
-//
-//        Group group = new Group(1, "Short", "Long");
-//        when(groupRepository.findById(group.getId())).thenReturn(Optional.of(group));
-//        ArrayList<Integer> userIds = new ArrayList<>();
-//        userIds.add(1);
-//        userIds.add(2);
-//
-//        groupService.addGroupMembers(group.getId(), userIds);
-//
-//        assertEquals(2, group.getUserList().size());
-//        assertEquals(userIds, group.getUserList());
-//
-//    }
-//
-//    @Test
-//    void testAddAlreadyPresentUser() {
-//
-//        Group group = new Group(1, "Short", "Long");
-//        when(groupRepository.findById(group.getId())).thenReturn(Optional.of(group));
-//        ArrayList<Integer> userIds = new ArrayList<>();
-//        userIds.add(1);
-//        userIds.add(2);
-//
-//        groupService.addGroupMembers(group.getId(), userIds);
-//        groupService.addGroupMembers(group.getId(), userIds);
-//
-//        assertEquals(2, group.getMemberIds().size());
-//        assertEquals(userIds, group.getMemberIds());
-//    }
-//
-//    @Test
-//    void testDeleteUser() {
-//
-//        Group group = new Group(1, "Short", "Long");
-//        when(groupRepository.findById(group.getId())).thenReturn(Optional.of(group));
-//        ArrayList<Integer> userIds = new ArrayList<>();
-//        userIds.add(1);
-//        userIds.add(2);
-//
-//        groupService.addGroupMembers(group.getId(), userIds);
-//        groupService.removeGroupMembers(group.getId(), userIds);
-//
-//        assertEquals(0, group.getMemberIds().size());
-//
-//    }
-//
-//
-//}
+package nz.ac.canterbury.seng302.identityprovider.service;
+
+import com.google.protobuf.Timestamp;
+import nz.ac.canterbury.seng302.identityprovider.User;
+import nz.ac.canterbury.seng302.identityprovider.UserRepository;
+import nz.ac.canterbury.seng302.identityprovider.groups.Group;
+import nz.ac.canterbury.seng302.identityprovider.groups.GroupRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+
+class GroupServiceTest {
+
+    private final GroupRepository groupRepository = Mockito.mock(GroupRepository.class);
+    private final UserRepository userRepository = Mockito.mock(UserRepository.class);
+    private GroupService groupService;
+
+    @BeforeEach
+    public void setUp() {
+        groupService = new GroupService(groupRepository, userRepository);
+    }
+
+    @Test
+    void testAddUser() {
+        User user = new User("Steve1", "password", "Steve", "Stevenson", "McSteve", "KingSteve", "", "", "Steve@steve.com", Timestamp.newBuilder().build());
+        User user2 = new User("Steve2", "password", "Steve", "Stevenson", "McSteve", "KingSteve", "", "", "Steve@steve.com", Timestamp.newBuilder().build());
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        userList.add(user2);
+
+
+        Group group = new Group(1, "Short", "Long");
+        ReflectionTestUtils.setField(group, "userList", new ArrayList<>());
+        when(groupRepository.findById(group.getId())).thenReturn(Optional.of(group));
+        when(userRepository.findAllById(Mockito.any())).thenReturn(userList);
+        ArrayList<Integer> userIds = new ArrayList<>();
+        userIds.add(1);
+        userIds.add(2);
+
+        groupService.addGroupMembers(group.getId(), userIds);
+
+        assertEquals(2, group.getUserList().size());
+        assertEquals(userList, group.getUserList());
+    }
+
+    @Test
+    void testAddAlreadyPresentUser() {
+        User user = new User("Steve1", "password", "Steve", "Stevenson", "McSteve", "KingSteve", "", "", "Steve@steve.com", Timestamp.newBuilder().build());
+        User user2 = new User("Steve2", "password", "Steve", "Stevenson", "McSteve", "KingSteve", "", "", "Steve@steve.com", Timestamp.newBuilder().build());
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        userList.add(user2);
+
+        Group group = new Group(1, "Short", "Long");
+        ReflectionTestUtils.setField(group, "userList", new ArrayList<>());
+        when(groupRepository.findById(group.getId())).thenReturn(Optional.of(group));
+        when(userRepository.findAllById(Mockito.any())).thenReturn(userList);
+        ArrayList<Integer> userIds = new ArrayList<>();
+        userIds.add(1);
+        userIds.add(2);
+
+        groupService.addGroupMembers(group.getId(), userIds);
+        groupService.addGroupMembers(group.getId(), userIds);
+
+        assertEquals(2, group.getMembersNumber());
+        assertEquals(userList, group.getUserList());
+    }
+
+    @Test
+    void testDeleteUser() {
+        User user = new User("Steve1", "password", "Steve", "Stevenson", "McSteve", "KingSteve", "", "", "Steve@steve.com", Timestamp.newBuilder().build());
+        User user2 = new User("Steve2", "password", "Steve", "Stevenson", "McSteve", "KingSteve", "", "", "Steve@steve.com", Timestamp.newBuilder().build());
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        userList.add(user2);
+
+
+        Group group = new Group(1, "Short", "Long");
+        ReflectionTestUtils.setField(group, "userList", new ArrayList<>());
+        when(groupRepository.findById(group.getId())).thenReturn(Optional.of(group));
+        when(userRepository.findAllById(Mockito.any())).thenReturn(userList);
+        ArrayList<Integer> userIds = new ArrayList<>();
+        userIds.add(1);
+        userIds.add(2);
+
+        groupService.addGroupMembers(group.getId(), userIds);
+        groupService.removeGroupMembers(group.getId(), userIds);
+
+        assertEquals(0, group.getMembersNumber());
+
+    }
+
+
+}
