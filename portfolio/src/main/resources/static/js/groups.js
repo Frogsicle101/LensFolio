060b6448cc5c11fb002d3e3b08e711265a17b7f4
Harrison Martin <hma112@uclive.ac.nz>
2022-07-13 13:07:35 +1200

    Changed the way results are displayed for groups, they now display in a table format. #feature

--- portfolio/src/main/resources/static/js/groups.js
+++ portfolio/src/main/resources/static/js/groups.js
@@ -1,4 +1,8 @@
-
+$(document).ready( function() {
+    $("input[type=checkbox]").change(function () {
+        console.log("yee")
+    })
+})
 /**
  * When group div is clicked, the members for that group are retrieved.
  */
@@ -8,20 +12,32 @@ $(document).on("click", ".group", function () {
     displayGroupUsersList(groupId);
 })
 
+$(document).on("click", "#selectAllCheckboxGroups", function() {
+    $(".selectUserCheckboxGroups").prop("checked", $("#selectAllCheckboxGroups").prop("checked"))
+})
+
+
+
 function displayGroupUsersList(groupId) {
-    let membersContainer = $(".membersContainer")
+    let membersContainer = $("#groupTableBody")
     $.ajax({
         url: `group?groupId=${groupId}`,
         type: "GET",
         success: (response) => {
-            membersContainer.empty();
+            console.log(response)
+            $("#groupTableBody").empty();
             $("#groupInformationShortName").text(response.shortName);
             $("#groupInformationLongName").text(response.longName);
             for (let member in response.userList) {
                 membersContainer.append(
-                    `<div class="groupMember">
-                        <h3 class="userName">${response.userList[member].username}</h3>
-                    </div>`
+                    `
+                 <tr>
+                    <th scope="row"><input class="selectUserCheckboxGroups" type="checkbox"></th>
+                    <td>${response.userList[member].id}</td>
+                    <td>${response.userList[member].firstName}</td>
+                    <td>${response.userList[member].lastName}</td>
+                    <td>${response.userList[member].username}</td>
+                </tr>`
                 )}
         },
         error: (error) => {
