555b42134e5edaedb7359da5577d8a888a7b976e
Harrison Martin <hma112@uclive.ac.nz>
2022-07-19 09:01:39 +1200

    Added in documentation for groups.js #feature

--- portfolio/src/main/resources/static/js/groups.js
+++ portfolio/src/main/resources/static/js/groups.js
@@ -2,6 +2,79 @@ let controlDown = false;
 let shiftDown = false;
 let selectedGroupId;
 let lastSelectedRow;
+
+
+// ******************************* Functions *******************************
+
+/**
+ * Displays the options for what to do with selected users.
+ * @param show a boolean of if to show or hide
+ */
+function showOptions(show) {
+    if (show) {
+        $("#groupDisplayOptions").slideDown()
+    } else {
+        $("#groupDisplayOptions").slideUp()
+    }
+    $(".numSelected").text($(".selected").length + " Selected")
+}
+
+/**
+ * Helper function that uses the amount of selected users to determine if to call the showOptions function
+ */
+function checkToSeeIfHideOrShowOptions() {
+    let amountSelected = $(".selected").length
+    if (amountSelected > 0) {
+        showOptions(true)
+    } else {
+        showOptions(false)
+    }
+}
+
+/**
+ * Makes an ajax get call to the server and gets all the information for a particular group.
+ * Loops through the groups members and adds them to the table.
+ * @param groupId the id of the group to fetch
+ */
+function displayGroupUsersList(groupId) {
+    let membersContainer = $("#groupTableBody")
+    $.ajax({
+        url: `group?groupId=${groupId}`,
+        type: "GET",
+        success: (response) => {
+            $("#groupTableBody").empty();
+            $("#groupInformationShortName").text(response.shortName);
+            $("#groupInformationLongName").text(response.longName);
+            selectedGroupId = response.id;
+            for (let member in response.userList) {
+                let imageSource;
+                if (response.userList[member].imagePath.length === 0) {
+                    imageSource = "defaultProfile.png"
+                } else {
+                    imageSource = response.userList[member].imagePath
+                }
+                membersContainer.append(
+                    `<tr class="userRow" userId=${response.userList[member].id}>
+                    <td>${response.userList[member].id}</td>
+                    <td>
+                        <img src=${imageSource} alt="Profile image" class="profilePicGroupsList" id="userImage"> 
+                    </td>
+                    <td>${response.userList[member].firstName}</td>
+                    <td>${response.userList[member].lastName}</td>
+                    <td>${response.userList[member].username}</td>
+                </tr>`
+                )}
+            $("#groupInformationContainer").slideDown()
+        },
+        error: (error) => {
+            console.log(error);
+        }
+    })
+
+}
+
+// ******************************* Click listeners *******************************
+
 /**
  * When group div is clicked, the members for that group are retrieved.
  */
@@ -13,29 +86,11 @@ $(document).on("click", ".group", function () {
     $(this).closest(".group").addClass("focusOnGroup")
 })
 
-function showOptions(show) {
-    if (show) {
-        $("#groupDisplayOptions").slideDown()
-    } else {
-        $("#groupDisplayOptions").slideUp()
-    }
-    $(".numSelected").text($(".selected").length + " Selected")
-}
-
-
-function checkToSeeIfHideOrShowOptions() {
-    let amountSelected = $(".selected").length
-    if (amountSelected > 0) {
-        showOptions(true)
-    } else {
-        showOptions(false)
-    }
-}
-
-
+/**
+ * Ajax post request to the server for moving users from one group to another
+ */
 $(document).on("click", "#moveUsersButton", function() {
     let arrayOfIds = [];
-
     $(".selected").each(function() {
         arrayOfIds.push($(this).attr("userId"))
     })
@@ -51,8 +106,60 @@ $(document).on("click", "#moveUsersButton", function() {
     })
 })
 
+/**
+ * selects every single user in the group when the button is clicked.
+ */
+$(document).on("click", "#selectAllCheckboxGroups", function() {
+    let isChecked = $("#selectAllCheckboxGroups").prop("checked")
+    $(".selectUserCheckboxGroups").prop("checked", isChecked)
+    if (isChecked) {
+        $(".userRow").addClass("selected")
+    } else {
+        $(".userRow").removeClass("selected")
+    }
+    checkToSeeIfHideOrShowOptions()
 
+})
 
+/**
+ * handles all the functionality for selecting users.
+ * Allows for using shift+click and ctrl+click functionality.
+ */
+$(document).on("click", ".userRow", function() {
+    if (!controlDown && !shiftDown) {
+        $(".selected").each(function() {
+            $(this).removeClass("selected")
+            $(this).find("input[type=checkbox]").prop("checked", false)
+        })
+    }
+    if (shiftDown) {
+        let boundaries = []; // Boundaries in this case are the first user, and the last user, used to select everything between.
+        boundaries.push(lastSelectedRow)
+        boundaries.push($(this).attr("userId"));
+        boundaries[0] = parseInt(boundaries[0])
+        boundaries[1] = parseInt(boundaries[1])
+        boundaries = boundaries.sort()
+        $(".userRow").each(function() {
+            if ($(this).attr("userId") >= boundaries[0] && $(this).attr("userId") <= boundaries[1]) {
+                $(this).addClass("selected")
+                $(this).find("input[type=checkbox]").prop("checked", true)
+            }
+        })
+    } else {
+        if ($(this).hasClass("selected")) {
+            $(this).removeClass("selected")
+            $(this).find("input[type=checkbox]").prop("checked", false)
+        } else {
+            $(this).addClass("selected")
+            $(this).find("input[type=checkbox]").prop("checked", true)
+        }
+    }
+    lastSelectedRow = $(this).attr("userId")
+    checkToSeeIfHideOrShowOptions()
+
+})
+
+// ******************************* Keydown listeners *******************************
 
 $(document).keydown(function(event) {
     if (event.key === "Control") {
@@ -79,18 +186,7 @@ $(document).keyup(function(event) {
     }
 })
 
-
-$(document).on("click", "#selectAllCheckboxGroups", function() {
-    let isChecked = $("#selectAllCheckboxGroups").prop("checked")
-    $(".selectUserCheckboxGroups").prop("checked", isChecked)
-    if (isChecked) {
-        $(".userRow").addClass("selected")
-    } else {
-        $(".userRow").removeClass("selected")
-    }
-    checkToSeeIfHideOrShowOptions()
-
-})
+// ******************************* Change listeners *******************************
 
 $(document).on("change","input[type=checkbox]", function() {
     let tableRow = $(this).closest("tr")
@@ -103,77 +199,8 @@ $(document).on("change","input[type=checkbox]", function() {
 
 
 
-$(document).on("click", ".userRow", function() {
-    if (!controlDown && !shiftDown) {
-        $(".selected").each(function() {
-            $(this).removeClass("selected")
-            $(this).find("input[type=checkbox]").prop("checked", false)
-        })
-    }
 
-    if (shiftDown) {
-        let boundaries = [];
-        boundaries.push(lastSelectedRow)
-        boundaries.push($(this).attr("userId"));
-        boundaries[0] = parseInt(boundaries[0])
-        boundaries[1] = parseInt(boundaries[1])
-        boundaries = boundaries.sort()
-        $(".userRow").each(function() {
-            if ($(this).attr("userId") >= boundaries[0] && $(this).attr("userId") <= boundaries[1]) {
-                $(this).addClass("selected")
-                $(this).find("input[type=checkbox]").prop("checked", true)
-            }
-        })
-    } else {
-        if ($(this).hasClass("selected")) {
-            $(this).removeClass("selected")
-            $(this).find("input[type=checkbox]").prop("checked", false)
-        } else {
-            $(this).addClass("selected")
-            $(this).find("input[type=checkbox]").prop("checked", true)
-        }
-    }
 
-    lastSelectedRow = $(this).attr("userId")
-    checkToSeeIfHideOrShowOptions()
 
-})
-
-function displayGroupUsersList(groupId) {
-    let membersContainer = $("#groupTableBody")
-    $.ajax({
-        url: `group?groupId=${groupId}`,
-        type: "GET",
-        success: (response) => {
-            $("#groupTableBody").empty();
-            $("#groupInformationShortName").text(response.shortName);
-            $("#groupInformationLongName").text(response.longName);
-            selectedGroupId = response.id;
-            for (let member in response.userList) {
-                let imageSource;
-                if (response.userList[member].imagePath.length === 0) {
-                    imageSource = "defaultProfile.png"
-                } else {
-                    imageSource = response.userList[member].imagePath
-                }
-                membersContainer.append(
-                 `<tr class="userRow" userId=${response.userList[member].id}>
-                    <td>${response.userList[member].id}</td>
-                    <td>
-                        <img src=${imageSource} alt="Profile image" class="profilePicGroupsList" id="userImage"> 
-                    </td>
-                    <td>${response.userList[member].firstName}</td>
-                    <td>${response.userList[member].lastName}</td>
-                    <td>${response.userList[member].username}</td>
-                </tr>`
-                )}
-            $("#groupInformationContainer").slideDown()
-        },
-        error: (error) => {
-            console.log(error);
-        }
-    })
-
-}
 
 
