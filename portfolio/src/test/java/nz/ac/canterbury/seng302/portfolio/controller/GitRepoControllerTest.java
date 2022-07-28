122250c5e43ddd588cad048560e900c4b43e378b
April Clarke <abc56@uclive.ac.nz>
2022-07-28 12:22:12 +1200

    Added authorisation check to the GitRepoController, so only members of the group can add git credentials. Also added tests for this authorisation. #feature #test

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/GitRepoControllerTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/GitRepoControllerTest.java
@@ -1,8 +1,12 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import nz.ac.canterbury.seng302.portfolio.authentication.Authentication;
 import nz.ac.canterbury.seng302.portfolio.projects.repositories.GitRepoRepository;
+import nz.ac.canterbury.seng302.portfolio.service.AuthenticateClientService;
 import nz.ac.canterbury.seng302.portfolio.service.GroupsClientService;
-import nz.ac.canterbury.seng302.shared.identityprovider.GroupDetailsResponse;
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountsClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -10,10 +14,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,6 +30,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(controllers = GitRepoController.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class GitRepoControllerTest {
+
+    private Authentication principal;
+    private GroupDetailsResponse response;
+    private UserResponse userResponse;
+
+    @MockBean
+    AuthenticateClientService authenticateClientService;
+
+    @MockBean
+    UserAccountsClientService userAccountsClientService;
 
     @MockBean
     GroupsClientService groupsClientService;
@@ -32,11 +51,39 @@ public class GitRepoControllerTest {
     private MockMvc mockMvc;
 
 
+    @BeforeEach
+    public void beforeAll() {
+        principal = new Authentication(AuthState.newBuilder()
+                .setIsAuthenticated(true)
+                .setNameClaimType("name")
+                .setRoleClaimType("role")
+                .addClaims(ClaimDTO.newBuilder().setType("nameid").setValue("1").build())
+                .addClaims(ClaimDTO.newBuilder().setType("role").setValue("course_administrator").build())
+                .build());
+
+        UserResponse.Builder userBuilder = UserResponse.newBuilder()
+                .setUsername("steve")
+                .setFirstName("Steve")
+                .setMiddleName("McSteve")
+                .setLastName("Steveson")
+                .setNickname("Stev")
+                .setBio("kdsflkdjf")
+                .setPersonalPronouns("Steve/Steve")
+                .setEmail("steve@example.com")
+                .setProfileImagePath("a");
+        userBuilder.addRoles(UserRole.STUDENT);
+        userResponse = userBuilder.build();
+
+        when(PrincipalAttributes.getUserFromPrincipal(principal.getAuthState(), userAccountsClientService)).thenReturn(userResponse);
+        Mockito.when(authenticateClientService.checkAuthState()).thenReturn(principal.getAuthState());
+
+        setupContext();
+    }
+
+
     @Test
     void testAddGitRepoValid() throws Exception {
-        GroupDetailsResponse response = GroupDetailsResponse.newBuilder().build();
-
-        Mockito.when(groupsClientService.getGroupDetails(any())).thenReturn(response);
+        setUserToGroupMember();
 
         mockMvc.perform(post("/addGitRepo")
                         .param("groupId", "1")
@@ -48,10 +95,21 @@ public class GitRepoControllerTest {
 
 
     @Test
-    void testAddGitRepoInvalidGroupId() throws Exception {
-        GroupDetailsResponse response = GroupDetailsResponse.newBuilder().build();
+    void testAddGitRepoInvalidUser() throws Exception {
+        setUserToNotGroupMember();
 
-        Mockito.when(groupsClientService.getGroupDetails(any())).thenReturn(response);
+        mockMvc.perform(post("/addGitRepo")
+                        .param("groupId", "1")
+                        .param("projectId", "1")
+                        .param("alias", "repo alias")
+                        .param("accessToken", "abcdef0123456789abcdef0123456789abcdef01"))
+                .andExpect(status().isUnauthorized());
+    }
+
+
+    @Test
+    void testAddGitRepoInvalidGroupId() throws Exception {
+        setUserToGroupMember();
 
         mockMvc.perform(post("/addGitRepo")
                         .param("groupId", "invalid")
@@ -64,9 +122,7 @@ public class GitRepoControllerTest {
 
     @Test
     void testAddGitRepoInvalidProjectId() throws Exception {
-        GroupDetailsResponse response = GroupDetailsResponse.newBuilder().build();
-
-        Mockito.when(groupsClientService.getGroupDetails(any())).thenReturn(response);
+        setUserToGroupMember();
 
         mockMvc.perform(post("/addGitRepo")
                         .param("groupId", "1")
@@ -79,9 +135,7 @@ public class GitRepoControllerTest {
 
     @Test
     void testAddGitRepoInvalidAlias() throws Exception {
-        GroupDetailsResponse response = GroupDetailsResponse.newBuilder().build();
-
-        Mockito.when(groupsClientService.getGroupDetails(any())).thenReturn(response);
+        setUserToGroupMember();
 
         mockMvc.perform(post("/addGitRepo")
                         .param("groupId", "1")
@@ -94,9 +148,7 @@ public class GitRepoControllerTest {
 
     @Test
     void testAddGitRepoNoAccessToken() throws Exception {
-        GroupDetailsResponse response = GroupDetailsResponse.newBuilder().build();
-
-        Mockito.when(groupsClientService.getGroupDetails(any())).thenReturn(response);
+        setUserToGroupMember();
 
         mockMvc.perform(post("/addGitRepo")
                         .param("groupId", "1")
@@ -109,9 +161,7 @@ public class GitRepoControllerTest {
 
     @Test
     void testAddGitRepoInvalidAccessTokenCharacters() throws Exception {
-        GroupDetailsResponse response = GroupDetailsResponse.newBuilder().build();
-
-        Mockito.when(groupsClientService.getGroupDetails(any())).thenReturn(response);
+        setUserToGroupMember();
 
         mockMvc.perform(post("/addGitRepo")
                         .param("groupId", "1")
@@ -119,5 +169,35 @@ public class GitRepoControllerTest {
                         .param("alias", "repo alias")
                         .param("accessToken", "usingIllegalChactersInTheAccesTokenHere!"))
                 .andExpect(status().isBadRequest());
+    }
+
+
+    private void setUserToGroupMember() {
+        response = GroupDetailsResponse.newBuilder()
+                .addMembers(userResponse).build();
+
+        Mockito.when(groupsClientService.getGroupDetails(any())).thenReturn(response);
+    }
+
+
+    private void setUserToNotGroupMember() {
+        UserResponse emptyUserResponse = UserResponse.newBuilder().build();
+
+        response = GroupDetailsResponse.newBuilder()
+                .addMembers(emptyUserResponse).build();
+
+        Mockito.when(groupsClientService.getGroupDetails(any())).thenReturn(response);
+    }
+
+
+    private void setupContext() {
+        Mockito.when(authenticateClientService.checkAuthState()).thenReturn(principal.getAuthState());
+
+        SecurityContext mockedSecurityContext = Mockito.mock(SecurityContext.class);
+
+        Mockito.when(mockedSecurityContext.getAuthentication())
+                .thenReturn(new PreAuthenticatedAuthenticationToken(principal, ""));
+
+        SecurityContextHolder.setContext(mockedSecurityContext);
     }
 }
