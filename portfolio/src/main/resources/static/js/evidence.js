9d4f0cb4e930659f589491d1772b8cd56e644eb4
Oliver Johnson <ojo26@uclive.ac.nz>
2022-08-03 16:45:51 +1200

    Added the weblink adding button. For now, it doesn't do anything. #feature

--- portfolio/src/main/resources/static/js/evidence.js
+++ portfolio/src/main/resources/static/js/evidence.js
@@ -74,6 +74,28 @@ $(document).on("click", "#evidenceSaveButton", function () {
     })
 })
 
+/**
+ * Listens for when add web link button is clicked.
+ * Slide-toggles the web link portion of the form.
+ */
+$(document).on('click', '.addWebbLinkButton', function () {
+    $(".weblink-form").slideToggle();
+    let button = $(".addWebbLinkButton");
+    if (button.hasClass("toggled")) {
+        //Un-toggle the button
+        button.text("Add Web Link")
+        button.removeClass("toggled")
+        button.removeClass("btn-primary")
+        button.addClass("btn-secondary")
+        //TODO: Add the code for submitting the web link here
+    } else {
+        button.text("Save Web Link")
+        button.addClass("toggled")
+        button.removeClass("btn-secondary")
+        button.addClass("btn-primary")
+    }
+})
+
 
 /**
  * Creates and returns an HTML element for an evidence preview
