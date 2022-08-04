2768a803711c1511932cddb5c4030e802d803e21
dzh80 <Jinchanzi8!>
2022-08-04 14:17:20 +1200

    fix some problem,now the repo info can be display for Team200, the group id is 35 #fix #feature #[dzh80]

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/PortfolioController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/PortfolioController.java
@@ -686,7 +686,7 @@ public class PortfolioController {
 
 
     public void createDefaultRepos(Project project){
-        GitRepository repo1 = new GitRepository(3, 13661, "Team 100's git Repository", "szMkVx_xM39gB5yRxSmL");
+        GitRepository repo1 = new GitRepository(35, 13661, "Team 100's git Repository", "szMkVx_xM39gB5yRxSmL");
         gitRepoRepository.save(repo1);
     }
 }
