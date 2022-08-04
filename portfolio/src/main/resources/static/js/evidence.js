310aed146e928542844f584473c577de746c147e
Oliver Johnson <ojo26@uclive.ac.nz>
2022-08-04 15:30:03 +1200

    Formatted the weblinks to have an icon, and for their tooltip to cut off the protocol. #feature #frontend

--- portfolio/src/main/resources/static/js/evidence.js
+++ portfolio/src/main/resources/static/js/evidence.js
@@ -274,12 +274,53 @@ function submitWebLink() {
 
     webLinkTitle.show()
     addedWebLinks.append(
-        ` <div class="addedWebLinkName" data-bs-toggle="tooltip" data-bs-placement="top"
-               data-bs-title="${address}" data-bs-custom-class="webLinkTooltip">${alias}
-          </div>`
+        webLinkElement(address, alias)
     )
 
     $('[data-bs-toggle="tooltip"]').tooltip(); //re-init tooltips so appended tooltip displays
+}
+
+
+/**
+ * Given a web address and an alias, creates and returns a web link element.
+ * The main div will have the class 'secured' if it is https, or 'unsecured' otherwise
+ *
+ * If the address doesn't start with https, it will show an un-filled, unlocked icon.
+ * If it does, it will show a locked, filled icon.
+ *
+ * @param address The web address of the web link
+ * @param alias The alias/nickname of the web address. Everything before the first // occurrence will be cut off
+ * (e.g. https://www.goggle.com becomes www.google.com)
+ * @returns {string} A single-div webLink element, wrapped in ` - e.g. `<div>stuff!</div>`
+ */
+function webLinkElement(address, alias) {
+    let icon;
+    let security = "unsecured"
+    if (address.startsWith("https://")) {
+        security = "secured"
+        icon = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill lockIcon" viewBox="0 0 16 16">
+        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+        </svg>
+        `
+    } else {
+        icon = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-unlock lockIcon" viewBox="0 0 16 16">
+        <path d="M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2zM3 8a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1H3z"/>
+        </svg>
+        `
+    }
+    let slashIndex = address.search("//") + 2
+    if (slashIndex > 1) address = address.slice(slashIndex) // Cut off the http:// or whatever else it might be
+    return (`
+<div class="webLinkElement ${security}" data-bs-toggle="tooltip" data-bs-placement="top" 
+    data-bs-title="${address}" data-bs-custom-class="webLinkTooltip">
+    ${icon}
+    <div class="addedWebLinkName">
+        ${alias}
+    </div>
+</div>
+    `)
 }
 
 
