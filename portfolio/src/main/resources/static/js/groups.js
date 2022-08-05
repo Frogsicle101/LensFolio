2af4330d28273a8b91df4519221014e25d6023be
Sam Clark <scl113@uclive.ac.nz>
2022-08-05 19:18:32 +1200

    #Review #Fix tested the finished git repo story and made bug fix

--- portfolio/src/main/resources/static/js/groups.js
+++ portfolio/src/main/resources/static/js/groups.js
@@ -169,11 +169,12 @@ function addUsers(groupId) {
  * @param show a boolean of if to show or hide
  */
 function showOptions(show) {
-    if ($("#groupDisplayOptions").is(':hidden')) {
+    let groupsDisplayOptions = $("#groupDisplayOptions")
+    if (groupsDisplayOptions.is(':hidden')) {
         if (show && (selectedGroupId !== TEACHER_GROUP_ID || isAdmin())) {
-            $("#groupDisplayOptions").slideDown()
+            groupsDisplayOptions.slideDown()
         } else {
-            $("#groupDisplayOptions").slideUp()
+            groupsDisplayOptions.slideUp()
         }
 
     }
@@ -314,7 +315,20 @@ function displayGroupRepoInformation () {
             if (response.length === 0){
                 repoInformationContainer.empty();
                 repoInformationContainer.append(`
-                    <h3 id="groupSettingsPageRepoName">No Repository</h3>`
+                        <div id="groupSettingsRepoInformationSection">
+                            <div id="groupSettingsRepoHeader">
+                                <h3 id="groupSettingsPageRepoName">No Repository</h3>
+                                <button type="button" class="editRepo noStyleButton marginSides1" data-bs-toggle="tooltip"
+                                        data-bs-placement="top" title="Edit Repository Settings">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                         class="bi bi-wrench-adjustable-circle" viewBox="0 0 16 16">
+                                        <path d="M12.496 8a4.491 4.491 0 0 1-1.703 3.526L9.497 8.5l2.959-1.11c.027.2.04.403.04.61Z"/>
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-1 0a7 7 0 1 0-13.202 3.249l1.988-1.657a4.5 4.5 0 0 1 7.537-4.623L7.497 6.5l1 2.5 1.333 3.11c-.56.251-1.18.39-1.833.39a4.49 4.49 0 0 1-1.592-.29L4.747 14.2A7 7 0 0 0 15 8Zm-8.295.139a.25.25 0 0 0-.288-.376l-1.5.5.159.474.808-.27-.595.894a.25.25 0 0 0 .287.376l.808-.27-.595.894a.25.25 0 0 0 .287.376l1.5-.5-.159-.474-.808.27.596-.894a.25.25 0 0 0-.288-.376l-.808.27.596-.894Z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div id="repoSettingsContainer"></div>
+                        </div>`
                 )
             }
             else {
@@ -374,12 +388,11 @@ function getRepoCommits() {
                     commitContainer.append(commitText)
                 }
 
+                commitContainer.append(`<h5>Recent Commits:</h5>`)
                 const firstThree = data.slice(0, 3);
-
                 for (let commit of firstThree) {
                     let commitText =
-                        `<h5>Recent Commits:</h5>
-                         <div id="groupSettingsCommitContainer" class="marginSides1">
+                        `<div id="groupSettingsCommitContainer" class="marginSides1">
                             <div class="gitCommitInfo">
                                 <div class="row">
                                     <div class="inlineText">
