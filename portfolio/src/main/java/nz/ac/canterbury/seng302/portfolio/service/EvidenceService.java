ce9a047b89e7a6753b2855358423e3891d912350
April Clarke <abc56@uclive.ac.nz>
2022-08-09 03:34:42 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/EvidenceService.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/EvidenceService.java
@@ -136,7 +136,7 @@ public class EvidenceService {
         checkString(description, StringType.DESCRIPTION);
 
         Evidence evidence = new Evidence(user.getId(), title, localDate, description);
-        evidenceRepository.save(evidence);
+        evidence = evidenceRepository.save(evidence);
 
         for (WebLinkDTO dto : webLinks) {
             WebLink webLink = new WebLink(evidence, dto.getName(), dto.getUrl());
