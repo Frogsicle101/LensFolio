57527cedafc7be65310b72f935d457f85dce3632
Dongjin Zhou <dzh80@uclive.ac.nz>
2022-08-20 15:12:30 +1200

    The front-end automated test navigates to the group page, and the rest is not According to Frederik's feedback in the review, the input of 10 web links is presented in a loop, and the problem that the test can still pass the test less than 10 times has been fixed. #test #[dzh80]

--- cypress/e2e/portfolioTest/WebLinkTest.cy.js
+++ cypress/e2e/portfolioTest/WebLinkTest.cy.js
@@ -12,88 +12,16 @@ context("WebLinkTest", () => {
 
     it('Up to 10 WebLinks', () => {
 
-        // inport weblink 1
-        cy.get('.addWebLinkButton').click();
-        cy.get('#webLinkUrl')
-            .type('https://www.canterbury.ac.nz/')
-        cy.get('#webLinkName')
-            .type('WebLink 1')
-        cy.get('.addWebLinkButton').click();
+        for (let i = 1; i < 11; i++) {
+            cy.get('.addWebLinkButton').click();
+            cy.get('#webLinkUrl')
+                .type('https://www.canterbury.ac.nz/')
+            cy.get('#webLinkName')
+                .type('WebLink ' + i.toString())
+            cy.get('.addWebLinkButton').click();
+        }
 
-        // inport weblink 2
-        cy.get('.addWebLinkButton').click();
-        cy.get('#webLinkUrl')
-            .type('https://www.canterbury.ac.nz/')
-        cy.get('#webLinkName')
-            .type('WebLink 2')
-        cy.get('.addWebLinkButton').click();
-
-        // inport weblink 3
-        cy.get('.addWebLinkButton').click();
-        cy.get('#webLinkUrl')
-            .type('https://www.canterbury.ac.nz/')
-        cy.get('#webLinkName')
-            .type('WebLink 3')
-        cy.get('.addWebLinkButton').click();
-
-        // inport weblink 4
-        cy.get('.addWebLinkButton').click();
-        cy.get('#webLinkUrl')
-            .type('https://www.canterbury.ac.nz/')
-        cy.get('#webLinkName')
-            .type('WebLink 4')
-        cy.get('.addWebLinkButton').click();
-
-        // inport weblink 5
-        cy.get('.addWebLinkButton').click();
-        cy.get('#webLinkUrl')
-            .type('https://www.canterbury.ac.nz/')
-        cy.get('#webLinkName')
-            .type('WebLink 5')
-        cy.get('.addWebLinkButton').click();
-
-        // inport weblink 6
-        cy.get('.addWebLinkButton').click();
-        cy.get('#webLinkUrl')
-            .type('https://www.canterbury.ac.nz/')
-        cy.get('#webLinkName')
-            .type('WebLink 6')
-        cy.get('.addWebLinkButton').click();
-
-        // inport weblink 7
-        cy.get('.addWebLinkButton').click();
-        cy.get('#webLinkUrl')
-            .type('https://www.canterbury.ac.nz/')
-        cy.get('#webLinkName')
-            .type('WebLink 7')
-        cy.get('.addWebLinkButton').click();
-
-        // inport weblink 8
-        cy.get('.addWebLinkButton').click();
-        cy.get('#webLinkUrl')
-            .type('https://www.canterbury.ac.nz/')
-        cy.get('#webLinkName')
-            .type('WebLink 8')
-        cy.get('.addWebLinkButton').click();
-
-        // inport weblink 9
-        cy.get('.addWebLinkButton').click();
-        cy.get('#webLinkUrl')
-            .type('https://www.canterbury.ac.nz/')
-        cy.get('#webLinkName')
-            .type('WebLink 9')
-        cy.get('.addWebLinkButton').click();
-
-        // inport weblink 10
-        cy.get('.addWebLinkButton').click();
-        cy.get('#webLinkUrl')
-            .type('https://www.canterbury.ac.nz/')
-        cy.get('#webLinkName')
-            .type('WebLink 10')
-        cy.get('.addWebLinkButton').click();
-
-        // assertion
-        cy.get('#webLinkFull').should('have.text', '10 web links have been added');
+        cy.get('#webLinkFull').should('be.visible');
     })
 
     }
