23a4bbedc61ab3bdb5965d6ccbe2a9a53adf5e58
Dongjin Zhou <dzh80@uclive.ac.nz>
2022-08-21 13:22:46 +1200

    Add icons for milestones and deadlines in sprints. #feature #[dzh80]

--- portfolio/src/main/resources/static/js/events.js
+++ portfolio/src/main/resources/static/js/events.js
@@ -559,6 +559,10 @@ function appendMilestoneToSprint(elementToAppendTo, milestone) {
     let milestoneInSprint = `
                 <div class="row" >
                     <div class="milestoneInSprint milestoneInSprint${milestone.id}">
+                        <svg data-toggle="tooltip"
+                                   xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trophy-fill calendarOccasion" viewBox="-3 -3 20 20">
+                                   <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/>
+                                   </svg>
                         <p class="sprintMilestoneName text-truncate">${milestone.name} :&#160</p>
                         <p class="sprintMilestoneEnd">${milestone.endDateFormatted}</p>
                     </div>
@@ -609,6 +613,10 @@ function appendDeadlineToSprint(elementToAppendTo, deadline) {
     let deadlineInSprint = `
                 <div class="row" >
                     <div class="deadlineInSprint deadlineInSprint${deadline.id}">
+                        <svg data-toggle="tooltip"
+                                    xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-hourglass-split calendarOccasion" viewBox="-3 -3 20 20">
+                                    <path d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z"/>
+                                    </svg>
                         <p class="sprintDeadlineName text-truncate">${deadline.name}</p>
                         <p class="sprintDeadlineEnd">${deadline.endDateFormatted}</p>
                     </div>
