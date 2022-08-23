d3ee209fef678268d1dde1672f788f6ccec47855
Samuel Clark <scl113@uclive.ac.nz>
2022-08-23 20:46:10 +1200

    Revert "#fix attempted to force the build process to clean the gradle dependencies on the gitlab runner"
    This reverts commit a757cd6f2a22fa091a77d54e1693bb5b9ddb2d0a.

--- identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/IdentityproviderApplicationTests.java
+++ identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/IdentityproviderApplicationTests.java
@@ -1,9 +1,12 @@
 package nz.ac.canterbury.seng302.identityprovider;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class IdentityproviderApplicationTests {
 
     @Test
