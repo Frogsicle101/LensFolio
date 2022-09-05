af639b51e0e15488933a777bc0d7bbf9523ceaa7
Frederik Markwell <fma107@uclive.ac.nz>
2022-09-05 19:27:48 +1200

    Made some minor fixes and added cypress tests for this functionality #fix #test

--- cypress/e2e/portfolioTest/EditingSprint.cy.js
+++ cypress/e2e/portfolioTest/EditingSprint.cy.js
@@ -14,5 +14,10 @@ describe('Test Editing Sprint', () => {
         cy.get("#sprintName").clear();
         cy.get("#sprintName").should("have.css", "border-color", 'rgb(220, 53, 69)');
     })
+
+    it('Does not steal focus', () => {
+        cy.get("#sprintEndDate").type("1999-12-01").trigger("change")
+        cy.focused().should('have.id', 'sprintEndDate')
+    })
 })
 
