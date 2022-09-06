4c27239cc6f82134207f2475077cfda3c40a2641
Aidan Campbell <aca170@uclive.ac.nz>
2022-09-06 00:17:58 +0000

    Apply 10 suggestion(s) to 5 file(s)

--- portfolio/src/main/resources/static/js/groupCreate.js
+++ portfolio/src/main/resources/static/js/groupCreate.js
@@ -27,21 +27,4 @@ $(() => {
             }
         })
     })
-
-    /**
-     * Function that gets the maxlength of an input and lets the user know how many characters they have left.
-     */
-    function countCharacters() {
-        let maxlength = $(this).attr("maxLength")
-        let lengthOfCurrentInput = $(this).val().length;
-        let counter = maxlength - lengthOfCurrentInput;
-        let helper = $(this).next(".form-text"); //Gets the next div with a class that is form-text
-
-        //If one character remains, changes from "characters remaining" to "character remaining"
-        if (counter !== 1) {
-            helper.text(counter + " characters remaining")
-        } else {
-            helper.text(counter + " character remaining")
-        }
-    }
 })
