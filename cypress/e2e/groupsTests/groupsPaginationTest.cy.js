87e2d0f5c9e615ae7e245727f0b4f0bff82c1100
Harrison Martin <hma112@uclive.ac.nz>
2022-09-08 12:57:06 +1200

    Ran tests and they all worked.
    #feature #fix #test

--- cypress/e2e/groupsTests/groupsPaginationTest.cy.js
+++ cypress/e2e/groupsTests/groupsPaginationTest.cy.js
@@ -1,42 +1,42 @@
 context("Test group pagination", () => {
-    beforeEach(() => {
-        cy.adminLogin()
-        cy.viewport(1200,1024)
-    })
+        beforeEach(() => {
+            cy.adminLogin()
+            cy.viewport(1200,1024)
+        })
 
-    it('There should be a selector for groups', () => {
-        cy.visit('/groups')
-        cy.get('#groupDisplayAmountSelectionTop').should("be.visible")
-    })
+        it('There should be a selector for groups', () => {
+            cy.visit('/groups')
+            cy.get('#groupDisplayAmountSelectionTop').should("be.visible")
+        })
 
-    it('Check buttons disable themselves', () => {
-        cy.visit('/groups')
-        cy.get('#groupDisplayAmountSelectionTop').should("be.visible")
-        cy.contains('[class="page-link"]', "1").click()
-        cy.get(".groupFooterFirst").should("have.class", "disabled")
-        cy.get(".groupFooterPrevious").should("have.class", "disabled")
-        cy.get(".groupFooterNext").should("not.have.class", "disabled")
-        cy.get(".groupFooterLast").should("not.have.class", "disabled")
-        cy.contains('[class="page-link"]', "Last").click() // This sometimes fails, I am not sure what the problem is,
-        // but I have tested extensively on the front-end and can't find a reason this fails apart from maybe a Cypress issue?
-        cy.get(".groupFooterNext").should("have.class", "disabled")
-        cy.get(".groupFooterLast").should("have.class", "disabled")
-        cy.get(".groupFooterFirst").should("not.have.class", "disabled")
-        cy.get(".groupFooterPrevious").should("not.have.class", "disabled")
-    })
+        it('Check buttons disable themselves', () => {
+            cy.visit('/groups')
+            cy.get('#groupDisplayAmountSelectionTop').should("be.visible")
+            cy.contains('[class="page-link"]', "1").click()
+            cy.get(".groupFooterFirst").should("have.class", "disabled")
+            cy.get(".groupFooterPrevious").should("have.class", "disabled")
+            cy.get(".groupFooterNext").should("not.have.class", "disabled")
+            cy.get(".groupFooterLast").should("not.have.class", "disabled")
+            cy.contains('[class="page-link"]', "Last").click() // This sometimes fails, I am not sure what the problem is,
+            // but I have tested extensively on the front-end and can't find a reason this fails apart from maybe a Cypress issue?
+            cy.get(".groupFooterNext").should("have.class", "disabled")
+            cy.get(".groupFooterLast").should("have.class", "disabled")
+            cy.get(".groupFooterFirst").should("not.have.class", "disabled")
+            cy.get(".groupFooterPrevious").should("not.have.class", "disabled")
+        })
 
-    it('Check amount of groups to display changes', () => {
-        cy.visit('/groups')
-        cy.contains('[class="page-link"]', "1").click()
-        cy.get(".scrollableGroupOverview").scrollTo("top")
-        cy.get("#groupDisplayAmountSelectionTop").select("10")
-        cy.get(".group ").should("have.length", 10)
-        cy.get(".scrollableGroupOverview").scrollTo("top")
-        cy.get("#groupDisplayAmountSelectionTop").select("20").wait(1000)
-        cy.contains('[class="page-link"]', "1").click()
-        cy.get(".group ").should("have.length", 20)
-        cy.get("#groupDisplayAmountSelectionTop").select("All")
-        cy.get(".group ").should("have.length.at.least", 70)
-    })
+        it('Check amount of groups to display changes', () => {
+            cy.visit('/groups')
+            cy.contains('[class="page-link"]', "1").click()
+            cy.get(".scrollableGroupOverview").scrollTo("top")
+            cy.get("#groupDisplayAmountSelectionTop").select("10")
+            cy.get(".group ").should("have.length", 10)
+            cy.get(".scrollableGroupOverview").scrollTo("top")
+            cy.get("#groupDisplayAmountSelectionTop").select("20").wait(1000)
+            cy.contains('[class="page-link"]', "1").click()
+            cy.get(".group ").should("have.length", 20)
+            cy.get("#groupDisplayAmountSelectionTop").select("All")
+            cy.get(".group ").should("have.length.at.least", 70)
+        })
     }
 )
