83218f6541942ead2dc9bbb12abea79c57b6b2df
Aidan Campbell <aca170@uclive.ac.nz>
2022-09-10 11:04:36 +1200

    I wrote some tests for linking users to pieces of evidence. I also managed to log in programmatically for the cypress tests so we don't have to log in through the ui. Lastly I made some changes to some of the tests to be more consistent #test #fix

---
+++ cypress/e2e/evidenceTests/LinkedUsersTest.cy.js
@@ -0,0 +1,30 @@
+describe('Link users to evidence', () => {
+    beforeEach(() => {
+        cy.adminLogin()
+        cy.viewport(1024, 1200)
+        cy.visit('/evidence')
+        cy.get("#createEvidenceButton").click()
+    })
+
+    it("can display linking users input", () => {
+        cy.get("#linkUsersToEvidenceButton").click()
+        cy.get("#linkUsersInput").should("be.visible")
+    })
+
+    it("can link users", () => {
+        cy.get("#linkUsersToEvidenceButton").click()
+        cy.get("#linkUsersInput").type("Joh").wait(2000).type('{enter}')
+        cy.get("#linkedUsersTitle").should("be.visible")
+        cy.get("#linkedUserId31").should("exist")
+    })
+
+    it("can't link same user twice", () => {
+            cy.get("#linkUsersToEvidenceButton").click()
+            cy.get("#linkUsersInput").type("Joh").wait(2000).type('{enter}')
+            cy.get("#linkedUsersTitle").should("be.visible")
+            cy.get("#linkedUserId31").should("exist")
+            cy.get("#linkUsersInput").type("Joh").wait(2000).type('{enter}')
+            cy.get("#linkedUsersTitle").should("be.visible")
+            cy.get("#linkedUserId31").should('have.length', 1)
+        })
+})
