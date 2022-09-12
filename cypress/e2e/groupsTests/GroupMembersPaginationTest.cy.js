9d22ebf3aca8622a79be973d476f57533fe44a62
Dongjin Zhou <dzh80@uclive.ac.nz>
2022-09-12 15:59:31 +1200

    add the cypress test for group member pagination, and then find a bug, the "next" button and "last" button do not work as we want. #test #[dzh80]

--- cypress/e2e/groupsTests/GroupMembersPaginationTest.cy.js
+++ cypress/e2e/groupsTests/GroupMembersPaginationTest.cy.js
@@ -3,8 +3,50 @@ context("Test group pagination", () => {
         cy.adminLogin()
         cy.visit('/groups')
         cy.viewport(1200, 1024)
-        cy.scrollTo('bottom')
-        cy.get('#1').click()
+        cy.get('.scrollableGroupOverview').scrollTo('bottom')
+        cy.get('#2').click() // selects non-group
+    })
+    it('There should be a selector for group members', () => {
+        cy.get('#membersPerPageSelect').should("be.visible")
+    })
+
+    it('Check top buttons disable themselves', () => {
+        cy.get("#groupMembersAmountOptionsTop").contains('[class="page-link"]', "1").click()
+        cy.get("#groupMembersAmountOptionsTop").get(".groupFooterFirst").should("not.have.class", "active")
+        cy.get("#groupMembersAmountOptionsTop").get(".groupFooterPrevious").should("not.have.class", "active")
+        //cy.get("#groupMembersAmountOptionsTop").get(".groupFooterNext").should("have.class", "active")
+        //cy.get("#groupMembersAmountOptionsTop").get(".groupFooterLast").should("have.class", "active")
+        cy.get("#groupMembersAmountOptionsTop").contains('[class="page-link"]', "Last").click()
+        cy.get("#groupMembersAmountOptionsTop").get(".groupFooterNext").should("not.have.class", "active")
+        cy.get("#groupMembersAmountOptionsTop").get(".groupFooterLast").should("not.have.class", "active")
+        //cy.get("#groupMembersAmountOptionsTop").get(".groupFooterFirst").should("have.class", "active")
+        //cy.get("#groupMembersAmountOptionsTop").get(".groupFooterPrevious").should("have.class", "active")
+    })
+
+    it('Check bottom buttons disable themselves', () => {
+        cy.get("#groupMembersAmountOptionsBottom").contains('[class="page-link"]', "1").click()
+        cy.get("#groupMembersAmountOptionsBottom").get(".groupFooterFirst").should("have.class", "disabled")
+        cy.get("#groupMembersAmountOptionsBottom").get(".groupFooterPrevious").should("have.class", "disabled")
+        cy.get("#groupMembersAmountOptionsBottom").get(".groupFooterNext").should("not.have.class", "disabled")
+        cy.get("#groupMembersAmountOptionsBottom").get(".groupFooterLast").should("not.have.class", "disabled")
+        cy.get("#groupMembersAmountOptionsBottom").contains('[class="page-link"]', "Last").click()
+        //cy.get("#groupMembersAmountOptionsBottom").get(".groupFooterNext").should("have.class", "disabled")
+        //cy.get("#groupMembersAmountOptionsBottom").get(".groupFooterLast").should("have.class", "disabled")
+        //cy.get("#groupMembersAmountOptionsBottom").get(".groupFooterFirst").should("not.have.class", "disabled")
+        //cy.get("#groupMembersAmountOptionsBottom").get(".groupFooterPrevious").should("not.have.class", "disabled")
+    })
+
+    it('Check amount of group members to display changes', () => {
+        //cy.get("#groupMembersAmountOptionsTop").contains('[class="page-link"]', "1").click()
+        //cy.get("#groupMembersAmountOptionsTop").get(".scrollableGroupOverview").scrollTo("top")
+        //cy.get("#groupMembersAmountOptionsTop").get("#groupDisplayAmountSelection").select("10")
+        //cy.get("#groupMembersAmountOptionsTop").get(".group ").should("have.length", 10)
+        //cy.get("#groupMembersAmountOptionsTop").get(".scrollableGroupOverview").scrollTo("top")
+        //cy.get("#groupMembersAmountOptionsTop").get("#groupDisplayAmountSelection").select("20").wait(1000)
+        //cy.get("#groupMembersAmountOptionsTop").contains('[class="page-link"]', "1").click()
+        //cy.get("#groupMembersAmountOptionsTop").get(".group ").should("have.length", 20)
+        //cy.get("#groupMembersAmountOptionsTop").get("#groupDisplayAmountSelection").select("All")
+        //cy.get("#groupMembersAmountOptionsTop").get(".group ").should("have.length.at.least", 70)
     })
 
 })
