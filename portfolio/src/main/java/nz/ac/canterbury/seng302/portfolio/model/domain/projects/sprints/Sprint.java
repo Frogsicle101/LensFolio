81ee337ea5089ce71082c5bd457a5ec65a3ec2cf
Sam Clark <scl113@uclive.ac.nz>
2022-09-21 21:08:17 +1200

    #Fix cleaned up the sprints class

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/domain/projects/sprints/Sprint.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/domain/projects/sprints/Sprint.java
@@ -10,22 +10,25 @@ import java.time.LocalDate;
 import java.util.Random;
 import java.util.UUID;
 
-
-@Entity // This maps Sprint to a table in the Db called "Sprint"
+/**
+ * The entity for representing the sprints of a project.
+ */
+@Entity
 public class Sprint {
+
     @Id
     private String id; // @Id lets JPA know it's the objects ID
 
     @ManyToOne
     private Project project;
+
     private String name;
     private LocalDate startDate;
     private LocalDate endDate;
     private String description;
     private String colour;
 
-    protected Sprint() {
-    }
+    protected Sprint() {/* Required constructor for JPA */}
 
     /**
      * Constructor for Sprint
@@ -47,48 +50,55 @@ public class Sprint {
         this.colour = colour;
     }
 
+
+    /**
+     * Constructor used when a sprint is added that can't go for 3 weeks.
+     *
+     * @param project     Project the sprint belongs to.
+     * @param name        Name of sprint.
+     * @param startDate   Start date of sprint.
+     * @param endDate     End date of sprint (and project end date).
+     */
+    public Sprint(Project project, String name, LocalDate startDate, LocalDate endDate) {
+        this(project, name, startDate, endDate, "No Description", String.format("#%06x", new Random().nextInt(0xffffff + 1)));
+    }
+
+
     /**
      * Default Constructor for Sprint
      *
      * @param project Project the sprint belongs too.
      * @param name    Name of the Sprint.
+     * @param startDate The date the sprint starts.
      */
     public Sprint(Project project, String name, LocalDate startDate) {
-
-        this.id = UUID.randomUUID().toString();
-        this.project = project;
-        this.name = name;
-        this.startDate = startDate;
-        this.endDate = (startDate).plusWeeks(3);
-        this.description = "No description";
-        this.colour = "#f554f5";
-        Random random = new Random();
-        int nextInt = random.nextInt(0xffffff + 1);
-        this.colour = String.format("#%06x", nextInt);
-    }
-
-
-    public Sprint(Project project, String name, LocalDate startDate, LocalDate endDate) {
-
-        this.id = UUID.randomUUID().toString();
-        this.project = project;
-        this.name = name;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.description = "No description";
-        Random random = new Random();
-        int nextInt = random.nextInt(0xffffff + 1);
-        this.colour = String.format("#%06x", nextInt);
+        this(project, name, startDate, startDate.plusWeeks(3));
     }
 
     public LocalDate getStartDate() {
         return startDate;
     }
 
+    /**
+     * Helper method for thymeleaf to format the date nicely.
+     * (Ignore unused warning)
+     *
+     * @return A formatted string with the sprint start time.
+     */
     public String getStartDateFormatted() {
         return startDate.format(DateTimeService.dayDateMonthYear());
     }
 
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * Helper method for thymeleaf to format the date nicely.
+     * (Ignore unused warning)
+     *
+     * @return A formatted string with the sprint end time.
+     */
     public String getEndDateFormatted() {
         return endDate.format(DateTimeService.dayDateMonthYear());
     }
@@ -121,10 +131,6 @@ public class Sprint {
         this.startDate = startDate;
     }
 
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
     public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
     }
@@ -144,7 +150,4 @@ public class Sprint {
     public void setColour(String colour) {
         this.colour = colour;
     }
-
-
 }
-
