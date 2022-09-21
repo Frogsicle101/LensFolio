ff30809ad3125f0497cbe46a4262f3f5e83922ba
Aidan Campbell <aca170@uclive.ac.nz>
2022-09-21 19:33:35 +1200

    I've changed the account page to use bootstrap validation so that we can see all the errors at once and so that it is obvious what is missing #fix #feature

--- portfolio/src/main/resources/static/js/account.js
+++ portfolio/src/main/resources/static/js/account.js
@@ -9,6 +9,8 @@ $(() => {
     let bio = $("#bio")
     let personalPronouns = $("#personalPronouns")
     let email = $("#email")
+    let accountForm = $("#accountForm")
+    let passwordForm = $("#passwordChangeForm")
 
     function toggleEditForm() {
         let canDisable = $(".canDisable")
@@ -36,56 +38,72 @@ $(() => {
 
 
     // On account form submit
-    $("#accountForm").on("submit", (event) => {
+    accountForm.on("submit", (event) => {
         event.preventDefault(); // Prevents submit
-        let accountData = {
-            "firstname": firstname.val(),
-            "middlename": middlename.val(),
-            "lastname": lastname.val(),
-            "nickname": nickname.val(),
-            "bio": bio.val(),
-            "personalPronouns": personalPronouns.val(),
-            "email": email.val()
-        }
-
-        $.ajax({
-            url: "edit/details",
-            type: "post",
-            data: accountData,
-            success:  () => {
-                createAlert("Updated details successfully!", "success")
-                toggleEditForm()
-            },
-            error: function (error) {//Displays error in box on failure
-                createAlert(error.responseText, "failure")
+        if (accountForm[0].checkValidity()) {
+            let accountData = {
+                "firstname": firstname.val(),
+                "middlename": middlename.val(),
+                "lastname": lastname.val(),
+                "nickname": nickname.val(),
+                "bio": bio.val(),
+                "personalPronouns": personalPronouns.val(),
+                "email": email.val()
             }
-        })
+
+            $.ajax({
+                url: "edit/details",
+                type: "post",
+                data: accountData,
+                success:  () => {
+                    createAlert("Updated details successfully!", "success")
+                    toggleEditForm()
+                },
+                error: function (error) {//Displays error in box on failure
+                    createAlert(error.responseText, "failure")
+                }
+            })
+        } else {
+            event.stopPropagation();
+            const errorElements = accountForm.find(".form-control:invalid")
+            $('html, body').animate({
+                scrollTop: $(errorElements[0]).offset().top - 100
+            }, 50);
+        }
     })
 
 
     // On password change form submit
-    $("#passwordChangeForm").on( "submit", (event) => {
+    passwordForm.on( "submit", (event) => {
         event.preventDefault()
-        let data = {
-            "oldPassword": $("#OldPassword").val(),
-            "newPassword": $("#NewPassword").val(),
-            "confirmPassword": $("#ConfirmPassword").val()
-        }
-
-        $.ajax({
-            type: "post",
-            data: data,
-            url: "edit/password",
-            success: function () {
-                createAlert("Password Changed Successfully!", "success")
-                toggleEditForm()
-                $("#OldPassword").val('')
-                $("#NewPassword").val('')
-                $("#ConfirmPassword").val('')
-            },
-            error: function (error) { // Display errors in box on failure
-                createAlert(error.responseText, "failure")
+        if (passwordForm[0].checkValidity()) {
+            let data = {
+                "oldPassword": $("#OldPassword").val(),
+                "newPassword": $("#NewPassword").val(),
+                "confirmPassword": $("#ConfirmPassword").val()
             }
-        })
+
+            $.ajax({
+                type: "post",
+                data: data,
+                url: "edit/password",
+                success: function () {
+                    createAlert("Password Changed Successfully!", "success")
+                    toggleEditForm()
+                    $("#OldPassword").val('')
+                    $("#NewPassword").val('')
+                    $("#ConfirmPassword").val('')
+                },
+                error: function (error) { // Display errors in box on failure
+                    createAlert(error.responseText, "failure")
+                }
+            })
+        } else {
+            event.stopPropagation();
+            const errorElements = passwordForm.find(".form-control:invalid")
+            $('html, body').animate({
+                scrollTop: $(errorElements[0]).offset().top - 100
+            }, 50);
+        }
     })
 })
