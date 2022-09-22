68a3a4aaf6064e35ade3ed68e766c2f74835b790
Sam Clark <scl113@uclive.ac.nz>
2022-09-22 23:12:30 +1200

    Added regex to the Add Evidence modal. Changed the regex slightly, as there were console errors due to parentheses being interpreted unusually #feature #pair[abc56, scl113]

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/WeblinkRegex.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/WeblinkRegex.java
@@ -5,21 +5,21 @@ import java.util.regex.Pattern;
 public class WeblinkRegex {
     //from rfc https://www.ietf.org/rfc/rfc2396.txt
     private static final String PCT = "(%[0-9a-f][0-9a-f])";
-    private static final String RESERVED = "[;/?:@&=+$,]";
-    private static final String UNRESERVED = "[a-zA-Z0-9-_\\.!~*'()]";
+    private static final String RESERVED = ";\\/\\?:@&=\\+\\$,";
+    private static final String UNRESERVED = "a-zA-Z0-9[-]_\\.!~\\*'\\(\\)";
     private static final String URIC = RESERVED + UNRESERVED + PCT;
 
-    private static final String PATH_CHARS = "a-zA-Z0-9-\\._~!$&'()*+,;=:@" + PCT;
-    private static final String QUERY_CHARS = "[%s]=[%<s](&[%<s]=[%<s])*".formatted(URIC);
+    private static final String PATH_CHARS = "a-zA-Z0-9[-]\\._~!\\$&'\\(\\)\\*\\+,;=:@" + PCT;
 
     private static final String PROTOCOL = "(https?://)?";
-    private static final String DOMAIN = "[a-zA-Z0-9-~_]+(\\.[a-zA-Z0-9-~_]+)*";
+    private static final String DOMAIN = "([a-zA-Z0-9\\-~_]+(\\.[a-zA-Z0-9\\-~_]+)*)";
     private static final String PORT = "(:[0-9]{1,5})?";
-    private static final String PATH = "(/[%s]*)*".formatted(PATH_CHARS);
-    private static final String QUERY = "(\\?[%s]+)?".formatted(QUERY_CHARS);
-    private static final String FRAGMENT = "(#[%s]*)?".formatted(URIC);
+    private static final String PATH = "([/]%s*)*".formatted(PATH_CHARS);
+    private static final String QUERY = "(\\?(((%s)+=(%<s))+(&%<s+=%<s+)*)+)?".formatted(URIC);
+    private static final String FRAGMENT = "(#%s*)?".formatted(URIC);
 
-    private static final Pattern WEBLINK = Pattern.compile(PROTOCOL +
+    private static final Pattern WEBLINK = Pattern.compile(
+            PROTOCOL +
             DOMAIN +
             PORT +
             PATH +
