5a3ec97687a0d82946ade373be24cd9d672cb162
Oliver Johnson <ojo26@uclive.ac.nz>
2022-09-22 16:46:28 +1200

    added cypress tests for sprint edit errors with invalid names/descriptions and another test for invalid dates. #test

--- cypress/e2e/portfolioTest/EditingProjectTest.cy.js
+++ cypress/e2e/portfolioTest/EditingProjectTest.cy.js
@@ -19,6 +19,28 @@ describe('Test Editing Project', () => {
         cy.focused().should('have.id', 'projectEndDate')
     })
 
+    it('Displays multiple errors without submission', () => {
+        cy.get("#projectName").invoke('val', 'An ℥ (ounce) of caution');
+        cy.get("#projectDescription").invoke('val', 'Ⅵ').trigger("input");
+
+        cy.get("#projectName").should("have.css", "border-color", 'rgb(220, 53, 69)');
+        cy.get("#projectDescription").should("have.css", "border-color", 'rgb(220, 53, 69)');
+
+        cy.get("#nameError").should("be.visible");
+        cy.get("#descriptionError").should("be.visible");
+    })
+
+    it('displays date errors', () => {
+        cy.get("#projectStartDate").invoke('removeAttr','type').clear().invoke('val', '03/04/2003');
+        cy.get("#projectEndDate").invoke('removeAttr','type').clear().invoke('val', '03/03/2022').trigger("change");
+
+        cy.get("#projectStartDate").should("have.css", "border-color", 'rgb(220, 53, 69)');
+        cy.get("#projectEndDate").should("have.css", "border-color", 'rgb(220, 53, 69)');
+
+        cy.get("#projectStartDateFeedback").should("be.visible");
+        cy.get("#projectEndDateFeedback").should("be.visible");
+    })
+
     it('Allows you to leave the description blank', () => {
         cy.get("#projectDescription").clear();
         cy.get("#projectDescription").should("have.css", "border-color", 'rgb(25, 135, 84)');
