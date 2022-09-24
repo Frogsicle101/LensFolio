d13288443ecb7fde86632649726b9fe1bd572150
=April Clarke <abc56@uclive.ac.nz>
2022-09-25 12:31:22 +1300

    Changed all icons to be consistent - pencil for edit and trash can for delete. Also refactored project.js slightly to extract unnecessary functionality from initialisation function #refactor

--- portfolio/src/main/resources/static/js/groups.js
+++ portfolio/src/main/resources/static/js/groups.js
@@ -761,10 +761,9 @@ function populateEmptyGroupRepo(container) {
                 <h3 id="groupSettingsPageRepoName">No Repository</h3>
                 <button type="button" class="editRepo noStyleButton marginSides1" data-bs-toggle="tooltip"
                         data-bs-placement="top" title="Edit Repository Settings">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                         class="bi bi-wrench-adjustable-circle" viewBox="0 0 16 16">
-                        <path d="M12.496 8a4.491 4.491 0 0 1-1.703 3.526L9.497 8.5l2.959-1.11c.027.2.04.403.04.61Z"/>
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-1 0a7 7 0 1 0-13.202 3.249l1.988-1.657a4.5 4.5 0 0 1 7.537-4.623L7.497 6.5l1 2.5 1.333 3.11c-.56.251-1.18.39-1.833.39a4.49 4.49 0 0 1-1.592-.29L4.747 14.2A7 7 0 0 0 15 8Zm-8.295.139a.25.25 0 0 0-.288-.376l-1.5.5.159.474.808-.27-.595.894a.25.25 0 0 0 .287.376l.808-.27-.595.894a.25.25 0 0 0 .287.376l1.5-.5-.159-.474-.808.27.596-.894a.25.25 0 0 0-.288-.376l-.808.27.596-.894Z"/>
+                    <svg class="bi bi-pencil" fill="currentColor" height="20" viewBox="0 0 16 16" width="20"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                     </svg>
                 </button>
             </div>
@@ -1061,7 +1060,7 @@ $(document).on("click", ".editButton", () => {
 
     let formControl = $(".form-control");
     formControl.each(countCharacters);
-    formControl.keyup(countCharacters);
+    formControl.on("keyup", countCharacters);
 })
 
 
