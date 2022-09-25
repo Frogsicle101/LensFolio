a093e45d139c364afa04dcdfdb91343d329cf1b7
dzh80 <Jinchanzi8!>
2022-09-25 15:46:27 +1300

    Add delete button to user linked. now there are some bugs. 1, evidence detail pages should not show the button.2, button should in same line with uer name.3, change the color from black to blue. #feature #[dzh80]

--- portfolio/src/main/resources/static/js/evidenceUtilities.js
+++ portfolio/src/main/resources/static/js/evidenceUtilities.js
@@ -93,11 +93,12 @@ function setHighlightedEvidenceWebLinks(response) {
     for (let index in response) {
         let webLink = response[index]
         webLinksDiv.append(webLinkElement(webLink.url, webLink.alias))
-        let deleteButtons = document.querySelectorAll('#deleteWeblink')
-        for (let i = 0; i < deleteButtons.length; i++) {
-            deleteButtons[i].style = "display: none;"
-        }
     }
+    let deleteButtons = document.querySelectorAll('#deleteWeblink')
+    for (let i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].style = "display: none;"
+    }
+
     if (webLinksDiv.children().length < 1) {
         $("#evidenceWebLinksBreakLine").hide()
     } else {
@@ -345,6 +346,9 @@ function addLinkedUsersToEvidence(users) {
     $.each(users, function (i, user) {
         linkedUsersDiv.append(linkedUserElement(user));
     })
+
+
+
 }
 
 
@@ -920,7 +924,16 @@ function addLinkedUser(user) {
  * Creates the element for displaying the linked user
  */
 function linkedUserElement(user) {
-    return `<div class="linkedUser" id="linkedUserId${user.id}" data-id="${user.id}">${user.firstName} ${user.lastName} (${user.username})</div>`
+    return `
+        <div>
+            <button id="deleteLinkedUser">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+                </button>
+            <div class="linkedUser" id="linkedUserId${user.id}" data-id="${user.id}">${user.firstName} ${user.lastName} (${user.username})</div>
+        </div>`
 }
 
 
