bc66a3063493ead47754b5c3c60cfa7a33885bd6
=April Clarke <abc56@uclive.ac.nz>
2022-09-25 17:02:39 +1300

    Simplified regex and fixed issue with mailto protocol being allowed #fix

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/WeblinkRegex.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/WeblinkRegex.java
@@ -8,19 +8,19 @@ public class WeblinkRegex {
     private static final String[] ALLOWED_PROTOCOLS = {"http", "https", "ftp"};
 
     //from rfc https://www.ietf.org/rfc/rfc2396.txt
-    private static final String ESCAPED = "(%[0-9a-f][0-9a-f])";
+    private static final String ESCAPED = "(%[\\da-f][\\da-f])";
     private static final String RESERVED = ";\\/\\?:@&=\\+\\$,";
-    private static final String UNRESERVED = "a-zA-Z0-9[-]_\\.!~\\*'\\(\\)";
+    private static final String UNRESERVED = "\\w[-]\\.!~\\*'\\(\\)";
     private static final String URIC = RESERVED + UNRESERVED + ESCAPED;
 
-    private static final String PATH_CHARS = "a-zA-Z0-9[-]\\._~!\\$&'\\(\\)\\*\\+,;=:@" + ESCAPED;
+    private static final String PATH_CHARS = "\\w[-]\\.~!\\$&'\\(\\)\\*\\+,;=:@/" + ESCAPED;
 
     private static final String PROTOCOL = "((%s)://)".formatted(String.join("|", ALLOWED_PROTOCOLS));
-    private static final String DOMAIN = "([a-zA-Z0-9\\-~_]+(\\.[a-zA-Z0-9\\-~_]+)*)";
-    private static final String PORT = "(:[0-9]{0,5})?";
-    private static final String PATH = "(/[%s]*)*".formatted(PATH_CHARS);
-    private static final String QUERY = "(\\?(([%s]+=[%<s]+)+(&[%<s]+=[%<s]+)*)+)?".formatted(URIC);
-    private static final String FRAGMENT = "(#[%s]*)?".formatted(URIC);
+    private static final String DOMAIN = "([\\w\\-~_]+(\\.[\\w\\-~]+)*)";
+    private static final String PORT = "(:\\d{0,5})?";
+    private static final String PATH = "(/(%s)*)*".formatted(PATH_CHARS);
+    private static final String QUERY = "(\\?(((%s)+=(%<s)+)+(&(%<s)+=(%<s)+)*)+)?".formatted(URIC);
+    private static final String FRAGMENT = "(#(%s)*)?".formatted(URIC);
 
     private static final Pattern WEBLINK = Pattern.compile(
                     PROTOCOL +
