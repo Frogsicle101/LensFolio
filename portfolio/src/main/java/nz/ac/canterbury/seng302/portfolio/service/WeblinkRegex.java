c709a2feb336f174b372cd04d2534004714cfa80
=April Clarke <abc56@uclive.ac.nz>
2022-09-25 17:43:39 +1300

    Added documentation to help debug issues with regex and fixed square brackets that were not understood by js

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/WeblinkRegex.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/WeblinkRegex.java
@@ -15,13 +15,52 @@ public class WeblinkRegex {
 
     private static final String PATH_CHARS = "\\w[-]\\.~!\\$&'\\(\\)\\*\\+,;=:@/" + ESCAPED;
 
+    /**
+     * The protocol can be any of the options in the ALLOWED_PROTOCOL array above.
+     * If a domain name is present, it must be followed by "://".
+     */
     private static final String PROTOCOL = "((%s)://)".formatted(String.join("|", ALLOWED_PROTOCOLS));
-    private static final String DOMAIN = "([\\w\\-~_]+(\\.[\\w\\-~]+)*)";
-    private static final String PORT = "(:\\d{0,5})?";
-    private static final String PATH = "(/(%s)*)*".formatted(PATH_CHARS);
-    private static final String QUERY = "(\\?(((%s)+=(%<s)+)+(&(%<s)+=(%<s)+)*)+)?".formatted(URIC);
-    private static final String FRAGMENT = "(#[%s]*)?".formatted(URIC);
 
+    /**
+     * The domain includes numbers, letters, and the special characters :-~_. .
+     * The "." character is only allowed after a non "." character has been used.
+     * There must be at least one domain character in the weblink.
+     */
+    private static final String DOMAIN = "([\\w\\-~_]+(\\.[\\w\\-~]+)*)";
+
+    /**
+     * The port starts with a ":" character.
+     * The colon is followed by between 0 and 5 digits.
+     * In theory, the max is 65535, but we have not been that specific.
+     * Optional.
+     */
+    private static final String PORT = "(:\\d{0,5})?";
+
+    /** The path starts with a "/", and is followed by any number of PATH_CHARS characters and "/" path delimiters.
+     * Optional.
+     */
+    private static final String PATH = "(/(%s)*)*".formatted(PATH_CHARS);
+
+    /**
+     * The query starts with a "?". It must be followed by at least one key-value assignment, and optionally more.
+     * Each additional key-value assignment is prefaced by an "&" character.
+     * Optional.
+     */
+    private static final String QUERY = "(\\?(((%s)+=(%<s)+)+(&(%<s)+=(%<s)+)*)+)?".formatted(URIC);
+
+    /**
+     * The fragment starts with a "#" character.
+     * It can contain numbers, letters, and special characters as defined in the URIC.
+     * Optional.
+     */
+    private static final String FRAGMENT = "(#(%s)*)?".formatted(URIC);
+
+
+    /**
+     * The compiled weblink pattern.
+     * If the protocol field is present, the domain, port, path, query, and fragment fields can also be present.
+     * Otherwise, only the domain and path are allowed. In this case, it is a web address.
+     */
     private static final Pattern WEBLINK = Pattern.compile(
                     PROTOCOL +
                     DOMAIN +
