adbc64ec945ecd43ae2aa4f860d285286bd33a9c
Dongjin Zhou <dzh80@uclive.ac.nz>
2022-09-27 12:26:33 +1300

    Fix NO SKILL show on evidence page. #fix #[dzh80]

--- portfolio/src/main/resources/static/js/evidenceBySkills.js
+++ portfolio/src/main/resources/static/js/evidenceBySkills.js
@@ -204,7 +204,8 @@ $(document).on("click", "#editEvidenceButton" , () => {
     let currentSkillsList = evidenceHighlight.querySelectorAll(".skillChip")
     for (let i = 0; i < currentSkillsList.length; i++) {
         let skillName = reversTranslationHTML(currentSkillsList[i].querySelector(".chipText").innerHTML)
-        let skillChip = `
+        if (skillName !== "No Skill") {
+            let skillChip = `
                 <div class="chip skillChip">
                     <p class="chipText">${sanitise(skillName)}</p>  
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle chipDelete" viewBox="0 0 16 16">
@@ -212,7 +213,8 @@ $(document).on("click", "#editEvidenceButton" , () => {
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                     </svg>
                 </div>`
-        document.getElementById("tagInputChips").innerHTML += skillChip;
+            document.getElementById("tagInputChips").innerHTML += skillChip;
+        }
     }
 
     //categories
