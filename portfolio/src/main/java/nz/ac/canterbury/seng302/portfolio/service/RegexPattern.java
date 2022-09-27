0bac78e869b0a6e5e5349dfe071b2ce06cd1bf8d
Frederik Markwell <fma107@uclive.ac.nz>
2022-09-28 11:24:29 +1300

    Added regular expression validation for access tokens #feature Added JUnit tests for this validation #test

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/RegexPattern.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/RegexPattern.java
@@ -37,7 +37,9 @@ public enum RegexPattern {
     /** Regex to check the titles of occasions, this should be checked. */
     OCCASION_TITLE(Pattern.compile("(\\w+\\s?)+"), " can only contain letters, numbers and spaces and must not start with whitespace."),
 
-    WEBLINK(WeblinkRegex.getWeblinkPattern(), " must be a valid URL.");
+    WEBLINK(WeblinkRegex.getWeblinkPattern(), " must be a valid URL."),
+
+    GITLAB_TOKEN(Pattern.compile("[A-Za-z0-9-_]{20}"), " should be a 20 character long string consisting of numbers, letters, hyphens, and underscores");
 
 
     // Enum attribute declaration
