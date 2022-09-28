c8d42731fb48019c9dccc61c29731181668615a7
Sam Clark <scl113@uclive.ac.nz>
2022-09-28 19:40:07 +1300

    #Fix removed unused function

--- portfolio/src/main/resources/static/js/evidenceUtilities.js
+++ portfolio/src/main/resources/static/js/evidenceUtilities.js
@@ -711,23 +711,6 @@ function getLinkedUsers() {
 
 
 /**
- * This function returns the html for the chips
- *
- * @param element the name of the skill
- * @returns {string} the html for the chip
- */
-function createDeletableSkillChip(element) {
-    return `<div class="chip skillChip">
-                <p class="chipText">${sanitise(element)}</p>  
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle chipDelete" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                </svg>
-            </div>`
-}
-
-
-/**
  * Due to a weird bug where the page would reload if you closed an alert if the
  * alert was open in a modal, this was added to stop the form from submitting which
  * seemed to be the cause of the issue.
