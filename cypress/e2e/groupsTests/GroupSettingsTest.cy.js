09cd3d80c586dae947ab0ed3285ba2ea9172040d
Frederik Markwell <fma107@uclive.ac.nz>
2022-09-29 09:37:36 +1300

    Implemented suggestions from review (adding backend validation, adding test for that) #feature #test

--- cypress/e2e/groupsTests/GroupSettingsTest.cy.js
+++ cypress/e2e/groupsTests/GroupSettingsTest.cy.js
@@ -4,11 +4,11 @@ context("Repo settings page", () => {
         cy.adminLogin()
         cy.visit('/groups')
         cy.get('#3').click()
+        cy.get("#pillsSettingsTab").click();
+        cy.get(".editRepo").click();
     })
 
     it('Counts alias characters correctly', () => {
-        cy.get("#pillsSettingsTab").click();
-        cy.get(".editRepo").click();
         cy.get("#repoName").clear();
         cy.get(".countChar").should("contain.text", "100");
 
@@ -16,6 +16,13 @@ context("Repo settings page", () => {
             cy.get("#repoName").type("a");
             cy.get(".countChar").should("contain.text", (100 - i).toString(10));
         }
+    })
+
+    it("Displays error when alias length bypassed", () => {
+        cy.get("#repoName").invoke("val", "w".repeat(10000));
+        cy.get("#editRepoForm").submit();
+        cy.get("#alertPopUpMessage").should("contain.text", "The repository name must be no longer than 100 characters")
+
     })
 
 })
